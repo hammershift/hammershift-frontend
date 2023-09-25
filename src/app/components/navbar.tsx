049b8837266530ac9b0caff7154044e45310d844
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-09-25 21:16:37 +0800

    feat: added content in navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import Logo from '../../../public/images/hammershift-logo.svg'
 import Image from 'next/image'
-import Button from '@mui/material/Button';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Navbar = () => {
   return (
-    <div>
-        <div>
-            <div>
+    <div className='tw-flex tw-py-2 tw-px-8 md:tw-px-32 tw-w-full'>
+        <div className='tw-flex tw-items-center tw-justify-between'>
+            <div className='tw-mx-4'>
                 <Image src={Logo} width={177} height={32} alt='logo'/>
             </div>
-            <div>DISCOVER</div>
-            <div>AUCTIONS</div>
+            <div className='tw-mx-4'>DISCOVER</div>
+            <div className='tw-mx-4'>AUCTIONS</div>
         </div>
-        <div>
-            <input/>
-            <Button variant="contained" color="primary" size='medium'>Create Account</Button>
+        <div className='tw-m-4 tw-flex tw-grow tw-items-center tw-justify-center'>
+            <input style={{backgroundColor: "#172431", padding: "0.5rem"}} className=' tw-w-2/3 tw-mx-10' placeholder='Search make, model, year...'></input>
+        </div>
+        <div className='tw-flex tw-items-center'>
+            <button className='btn-white'>CREATE ACCOUNT</button>
         </div>
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import "./styles/app.css";
 import Homepage from "./pages/homepage";
 
 export default function Home() {
