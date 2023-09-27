311663ea2bbdb2503f78b4e652981a3bf050303f
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-09-27 10:54:13 +0800

    feat: finished liveGamesCard

--- src/app/pages/homepage.tsx
+++ src/app/pages/homepage.tsx
@@ -8,6 +8,8 @@ import ArrowRight from '../../../public/images/arrow-right.svg'
 import ArrowLeft from '../../../public/images/arrow-left.svg'
 import TeamBattlesIcon from '../../../public/images/team-battles-icon.svg'
 import TournamentsIcon from '../../../public/images/tournaments-icon.svg'
+import HourGlassIcon from '../../../public/images/hour-glass.svg'
+import PlayersIcon from '../../../public/images/players.svg'
 
 
 const Homepage = () => {
@@ -56,7 +58,7 @@ const LiveGames = () => {
           <Image src={ArrowRight} width={32} height={32} alt="arrow right" className='tw-w-8 tw-h-8 tw-ml-4' />
         </div>
       </div>
-      <div>
+      <div className='tw-mt-6'>
         <LiveGamesCard />
         {/* carData.map((item) => {
             <LiveGamesCard key={item.id} url={item.url} year={item.year} name={item.name} description={item.description} time={item.time} />
@@ -74,16 +76,27 @@ const LiveGamesCard = () => {
     year: "1969",
     name: "Shelby Mustang",
     description: "GT350",
-    time: "12:17:00"
+    time: "12:17:00",
+    isLive: true
   }
 
   return (
-    <div className='tw-w-auto'>
-      <div>
-        <img src={carData.url} width={200} height={200} alt="car" className='tw-w-[200px] tw-h-[200px] tw-rounded-full tw-object-cover' />
+    <div className='tw-w-[200px] tw-flex tw-flex-col tw-items-center'>
+      <div className='tw-w-[200px] tw-h-[218px] tw-relative'>
+        <div className='tw-w-[61px] tw-h-[36px] tw-bg-red-500 tw-rounded-s-full tw-rounded-e-full tw-flex tw-justify-center tw-items-center tw-absolute tw-bottom-0 tw-left-[70px]'>LIVE</div>
+        <img src={carData.url} width={200} height={200} alt="car" className='tw-w-[200px] tw-h-[200px] tw-rounded-full tw-object-cover tw-border-solid tw-border-4 tw-border-red-500' />
       </div>
-      <div></div>
-      <div></div>
+      <div className='info tw-my-3 tw-flex tw-flex-col tw-items-center'>
+        <div className='tw-mt-3 tw-font-medium'>{carData.year} {carData.name}</div>
+        <div className='tw-my-1.5 tw-font-medium'>{carData.description}</div>
+        <div className='tw-flex tw-items-center'>
+          <Image src={HourGlassIcon} width={12} height={12} alt="hour glass" className='tw-w-[12px] tw-h-[14px] tw-mr-1 ' />
+          <div>{carData.time}</div>
+        </div>
+      </div>
+      <div className='tw-mt-1.5'>
+        <Image src={PlayersIcon} width={152} height={40} alt="players" className='' />
+      </div>
 
     </div>
   )
