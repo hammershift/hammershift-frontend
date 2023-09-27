6e552b36e489069e55061ed26e1b5b55d1dd6d67
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-09-27 16:18:19 +0800

    feat: created newEraWagering and GamesByMake sections

--- src/app/pages/homepage.tsx
+++ src/app/pages/homepage.tsx
@@ -10,6 +10,19 @@ import TeamBattlesIcon from '../../../public/images/team-battles-icon.svg'
 import TournamentsIcon from '../../../public/images/tournaments-icon.svg'
 import HourGlassIcon from '../../../public/images/hour-glass.svg'
 import PlayersIcon from '../../../public/images/players.svg'
+import GamesByMakeIcon from '../../../public/images/games-by-make-icon.svg'
+
+import BMWLogo from '../../../public/images/brand-logos/bmw-logo.svg'
+import AudiLogo from '../../../public/images/brand-logos/audi-logo.svg'
+import DodgeLogo from '../../../public/images/brand-logos/dodge-logo.svg'
+import HondaLogo from '../../../public/images/brand-logos/honda-logo.svg'
+import JeepLogo from '../../../public/images/brand-logos/jeep-logo.svg'
+import NissanLogo from '../../../public/images/brand-logos/nissan-logo.svg'
+import SubaruLogo from '../../../public/images/brand-logos/subaru-logo.svg'
+import TeslaLogo from '../../../public/images/brand-logos/tesla-logo.svg'
+import ToyotaLogo from '../../../public/images/brand-logos/toyota-logo.svg'
+import FordLogo from '../../../public/images/brand-logos/ford-logo.svg'
+
 
 
 const Homepage = () => {
@@ -19,14 +32,23 @@ const Homepage = () => {
       <div className='tw-mt-16'>
         <Carousel />
       </div>
-      <div className='tw-mt-24'>
+      <div className='tw-mt-16'>
         <LiveGames />
       </div>
-      <div className='tw-mt-24'>
+      <div className='tw-mt-16'>
         <TeamBattles />
       </div>
-      <div className='tw-mt-24'>
+      <div className='tw-mt-16'>
         <Tournaments />
+      </div>
+      <div className='tw-mt-16'>
+        <NewEraWagering />
+      </div>
+      <div className='tw-mt-16'>
+        <GamesByMake />
+      </div>
+      <div className='tw-mt-16'>
+        <WagerByCatergory />
       </div>
     </div>
   )
@@ -90,12 +112,12 @@ const LiveGamesCard = () => {
         <div className='tw-mt-3 tw-font-medium'>{carData.year} {carData.name}</div>
         <div className='tw-my-1.5 tw-font-medium'>{carData.description}</div>
         <div className='tw-flex tw-items-center'>
-          <Image src={HourGlassIcon} width={12} height={12} alt="hour glass" className='tw-w-[12px] tw-h-[14px] tw-mr-1 ' />
+          <Image src={HourGlassIcon} width={12} height={14} alt="hour glass" className='tw-w-[12px] tw-h-[14px] tw-mr-1 ' />
           <div>{carData.time}</div>
         </div>
       </div>
       <div className='tw-mt-1.5'>
-        <Image src={PlayersIcon} width={152} height={40} alt="players" className='' />
+        <Image src={PlayersIcon} width={152} height={40} alt="players" className='tw-w-[152px] tw-h-[40px]' />
       </div>
 
     </div>
@@ -134,6 +156,100 @@ const Tournaments = () => {
         <div className='tw-flex'>
           <Image src={ArrowLeft} width={32} height={32} alt="arrow left" className='tw-w-8 tw-h-8 ' />
           <Image src={ArrowRight} width={32} height={32} alt="arrow right" className='tw-w-8 tw-h-8 tw-ml-4' />
+        </div>
+      </div>
+      <div>
+        {/* insert content */}
+      </div>
+    </div>
+  )
+}
+
+const NewEraWagering = () => {
+  return (
+    <div className='tw-w-screen tw-bg-[#DCE0D9] tw-text-[#0F1923] tw-pb-[120px]'>
+      <div>
+        <div className='tw-h-[6px]'></div>
+        <div className='tw-bg-[#0F1923] tw-h-[40px]'></div>
+        <div className='tw-bg-[#0F1923] tw-h-[24px] tw-mt-3.5'></div>
+        <div className='tw-bg-[#0F1923] tw-h-[8px] tw-mt-8'></div>
+        <div className='tw-bg-[#0F1923] tw-h-[4px] tw-mt-12'></div>
+      </div>
+      <div className='tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-py-24 tw-px-4 md:tw-px-16 xl:tw-px-36'>
+        <div className='tw-relative'>
+          <div className=' tw-font-bold tw-text-[48px] md:tw-text-[60px] lg:tw-text-[80px]' >A New Era</div>
+          <div className=' tw-font-bold tw-text-[48px] md:tw-text-[60px] lg:tw-text-[80px]' >of Wagering</div>
+        </div>
+        <div>
+          <p>Excepteur sint obcaecat cupiditat non proident culpa. At nos hinc posthac, sitientis piros Afros. Cum sociis natoque penatibus et magnis dis parturient. Quam diu etiam furor iste tuus nos eludet?<br /><br />
+            Quam temere in vitiis, legem sancimus haerentia. Phasellus laoreet lorem vel dolor tempus vehicula. Qui ipsorum lingua Celtae, nostra Galli appellantur. Curabitur blandit tempus ardua ridiculus sed magna. Tu quoque, Brute, fili mi, nihil timor populi, nihil! Donec sed odio operae, eu vulputate felis rhoncus.</p>
+          <div className='tw-mt-6'>
+            <button className='btn-dark'>Sign up to win</button>
+            <button className='btn-transparent tw-ml-4'>About HammerShift</button>
+          </div>
+        </div>
+      </div>
+      <div className='options tw-grid tw-cols-1 md:tw-grid-cols-3 tw-gap-6 tw-px-4 md:tw-px-16 xl:tw-px-36'>
+        <div className=' tw-bg-white tw-rounded-lg tw-text-center tw-py-[32px] tw-px-[24px]'>
+          <Image src={LiveGamesIcon} width={68} height={68} alt="dollar" className='tw-block tw-mx-auto tw-w-[68px] tw-h-[68px] tw-shadow-lg tw-rounded-[16px] ' />
+          <h1 className='tw-font-bold tw-text-[24px] tw-mt-3'>Guess the Price</h1>
+          <p>Wager on the car auction and guess the final hammer price. Closest player wins the prize.</p>
+          <button className='btn-yellow tw-mt-4'>View games</button>
+        </div>
+        <div className='tw-bg-white tw-rounded-lg tw-text-center tw-py-[32px] tw-px-[24px]'>
+          <Image src={TeamBattlesIcon} width={68} height={68} alt="dollar" className='tw-block tw-mx-auto tw-w-[68px] tw-h-[68px] tw-shadow-lg tw-rounded-[16px]' />
+          <h1 className='tw-font-bold tw-text-[24px] tw-mt-3'>Team Battles</h1>
+          <p>Pick between teams betting on the same car. Player in the team with the closest wager wins the prize.</p>
+          <button className='btn-yellow  tw-mt-4'>Pick a team</button>
+        </div>
+        <div className='tw-bg-white tw-rounded-lg tw-text-center tw-py-[32px] tw-px-[24px]'>
+          <Image src={TournamentsIcon} width={68} height={68} alt="dollar" className='tw-block tw-mx-auto tw-w-[68px] tw-h-[68px] tw-shadow-lg tw-rounded-[16px]' />
+          <h1 className='tw-font-bold tw-text-[24px] tw-mt-3'>Tournaments</h1>
+          <p>Get more points the closer you are to the hammer price of a curated set of car auctions.</p>
+          <button className='btn-yellow tw-mt-4'>Buy-in for $100</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+const GamesByMake = () => {
+  // const carList = [BMWLogo, AudiLogo, DodgeLogo, FordLogo, HondaLogo, JeepLogo, NissanLogo, SubaruLogo, TeslaLogo, ToyotaLogo];
+  const carList = [{ name: BMWLogo, width: 100 }, { name: AudiLogo, width: 120 }, { name: DodgeLogo, width: 180 }, { name: FordLogo, width: 160 }, { name: HondaLogo, width: 120 }, { name: JeepLogo, width: 100 }, { name: NissanLogo, width: 120 }, { name: SubaruLogo, width: 120 }, { name: TeslaLogo, width: 160 }, { name: ToyotaLogo, width: 120 }]
+
+
+  return (
+    <div className='tw-px-4 md:tw-px-16 xl:tw-px-36 tw-w-screen tw-pt-8'>
+      <div className='tw-flex tw-justify-between'>
+        <div className='tw-flex tw-items-center'>
+          <Image src={GamesByMakeIcon} width={40} height={40} alt="dollar" className='tw-w-10 tw-h-10' />
+          <div className='tw-font-bold tw-text-2xl sm:tw-text-3xl tw-ml-4'>Games by Make</div>
+        </div>
+        <div className='tw-flex'>
+          <Image src={ArrowLeft} width={32} height={32} alt="arrow left" className='tw-w-8 tw-h-8 ' />
+          <Image src={ArrowRight} width={32} height={32} alt="arrow right" className='tw-w-8 tw-h-8 tw-ml-4' />
+        </div>
+      </div>
+      <div className='tw-grid tw-grid-cols-2 tw-grid-rows-5 md:tw-grid-cols-5 md:tw-grid-rows-2 tw-gap-8 tw-mt-16'>
+        {carList.map((car) => {
+          return <div key={car.name}>
+            <Image src={car.name} width={car.width} height={100} alt={car.name} style={{ width: car.width, height: "100px" }} className='tw-block tw-mx-auto' />
+          </div>
+        })}
+      </div>
+    </div>
+  )
+}
+
+
+const WagerByCatergory = () => {
+  return (
+    <div className='tw-px-4 md:tw-px-16 xl:tw-px-36 tw-w-screen tw-pt-8'>
+      <div>
+        <div className='tw-flex tw-items-center'>
+          <Image src={GamesByMakeIcon} width={40} height={40} alt="dollar" className='tw-w-10 tw-h-10' />
+          <div className='tw-font-bold tw-text-2xl sm:tw-text-3xl tw-ml-4'>Wager by Category</div>
         </div>
       </div>
       <div>
