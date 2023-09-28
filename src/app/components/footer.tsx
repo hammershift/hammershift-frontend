83e1da90ecad96d278e41101e7cd704bdcfb77b1
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-09-28 21:15:11 +0800

    feat: created initial design of footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import Logo from '../../../public/images/hammershift-logo.svg'
+import Image from 'next/image'
+
+const companyList = ["HOW IT WORKS", "ABOUT", "PRESS KIT", "PRIVACY NOTICE", "TERMS & CONDITIONS", "CONTRACT"];
+const productList = ["DISCOVER", "AUCTIONS", "NEWLY LISTED", "MOST EXPENSIVE", "MOST BIDS", "ENDING SOON", "MOST WAGERS",]
+const vehiclesList = ["MAKE", "TYPE", "ERA", "LOCATION"]
 
 const Footer = () => {
     return (
-        <div className='tw-px-4 md:tw-px-16 xl:tw-px-36 tw-w-screen tw-py-12'>Footer</div>
+        <div className='tw-px-4 md:tw-px-16 xl:tw-px-36 tw-w-screen tw-pt-[120px] tw-pb-20'>
+            <div className='tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-8 md:tw-gap-0 tw-mb-16'>
+                <div>
+                    <Image src={Logo} width={177} height={32} alt="hammershift logo" className='tw-w-[177px] tw-h-[32px]' />
+                </div>
+                <div className='tw-grid tw-grid-cols-1 md:tw-grid-cols-3 tw-gap-8 md:tw-gap-0'>
+                    <div>
+                        <div className='tw-font-bold'>COMPANY</div>
+                        {companyList.map((item) => {
+                            return <div key={item} className='tw-mt-3'>{item}</div>
+                        })}
+                    </div>
+                    <div>
+                        <div className='tw-font-bold'>PRODUCT</div>
+                        {productList.map((item) => {
+                            return <div key={item} className='tw-mt-3'>{item}</div>
+                        })}
+                    </div>
+                    <div>
+                        <div className='tw-font-bold'>VEHICLES</div>
+                        {vehiclesList.map((item) => {
+                            return <div key={item} className='tw-mt-3'>{item}</div>
+                        })}
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <div className='tw-flex tw-flex-col md:tw-flex-row tw-justify-between tw-items-end'>
+                <div className='tw-my-8'>
+                    <div className='tw-font-bold tw-text-xl'>The Ultimate Playground for Car Enthusiasts</div>
+                    <div>Skill-based wagering for those in the know</div>
+                    <div>{/* insert sm icons */}</div>
+
+                </div>
+                <div className='tw-flex tw-flex-col tw-items-end'>
+                    <div className='tw-opacity-20'>Designed by Toffeenut Design</div>
+                    <div>© 2023 HammerShift. All rights reserved.</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/app/components/stay_fast_lane.tsx
+++ src/app/components/stay_fast_lane.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const StayInTheFastLane = () => {
     return (
         <div className='tw-px-4 md:tw-px-16 xl:tw-px-36 tw-w-screen tw-py-32 tw-text-[#0F1923] tw-bg-[#F2CA16]'>
-            <div className='tw-grid tw-grid-cols-2'>
+            <div className='tw-grid tw-grid-cols-1 md:tw-grid-cols-2'>
                 <div>
                     <h1 className='tw-text-6xl tw-font-bold'>Stay in the Fast Lane</h1>
                     <p className='tw-my-10'>Are you ready to dive deeper into the captivating world of car auction wagering? Join the exclusive HammerShift newsletter to get access to a treasure trove of insider knowledge, expert tips, and captivating insights that will elevate your wagering game.</p>
