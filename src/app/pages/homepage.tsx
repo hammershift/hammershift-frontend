2e7b9754352fa588c362c640d827700f7987f7e6
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-09-29 13:32:00 +0800

    fix: completed Team battles section in homepage

--- src/app/pages/homepage.tsx
+++ src/app/pages/homepage.tsx
@@ -39,6 +39,11 @@ import YellowSportsCar from '../../../public/images/wager-by-category/yellow-spo
 import SilverPickup from '../../../public/images/wager-by-category/silver-pickup.svg'
 import SilverSUV from '../../../public/images/wager-by-category/silver-suv.svg'
 
+import AvatarOne from '../../../public/images/avatar-one.svg'
+import AvatarTwo from '../../../public/images/avatar-two.svg'
+import AvatarThree from '../../../public/images/avatar-three.svg'
+import AvatarFour from '../../../public/images/avatar-four.svg'
+
 
 
 interface carDataProps {
@@ -185,6 +190,28 @@ const LiveGamesCard: React.FC<LiveGamesCardProps> = ({ url, year, name, descript
 
 
 const TeamBattles = () => {
+  const teamPlayers = [{
+    id: "t1",
+    username: "Username",
+    avatar: AvatarOne,
+    amount: "$168,00"
+  }, {
+    id: "t2",
+    username: "Username",
+    avatar: AvatarTwo,
+    amount: "$157,00"
+  }, {
+    id: "t3",
+    username: "Username",
+    avatar: AvatarThree,
+    amount: "$152,00"
+  }, {
+    id: "t4",
+    username: "Username",
+    avatar: AvatarFour,
+    amount: "$132,00"
+  },
+  ]
   return (
     <div className='tw-px-4 md:tw-px-16 xl:tw-px-36 tw-w-screen tw-pt-8'>
       <header className='tw-flex tw-justify-between'>
@@ -197,7 +224,7 @@ const TeamBattles = () => {
           <Image src={ArrowRight} width={32} height={32} alt="arrow right" className='tw-w-8 tw-h-8 tw-ml-4' />
         </div>
       </header>
-      <section className='left-container tw-grid tw-grid-cols-1 sm:tw-grid-cols-2 tw-gap-8 tw-mt-16'>
+      <section className='left-container tw-grid tw-grid-cols-1 sm:tw-grid-cols-2 tw-gap-16 tw-mt-16'>
         <div
           style={{ backgroundImage: `url(https://images4.alphacoders.com/110/1103803.jpg)` }}
           className='tw-h-[388px] tw-w-auto tw-bg-cover tw-rounded-lg tw-p-4 tw-flex tw-flex-col tw-justify-end'>
@@ -207,27 +234,53 @@ const TeamBattles = () => {
             <div>12:17:00</div>
           </div>
         </div>
-        <div className='right-container tw-grid tw-grid-cols-1 sm:tw-grid-cols-2 tw-gap-4 tw-w-auto tw-h-auto'>
-
-          <div className='tw-relative '>
-            <div className='tw-px-5'>
+        <div className='right-container tw-grid tw-grid-cols-1 sm:tw-grid-cols-2 tw-gap-16 tw-w-auto tw-h-auto'>
+          {/* Team A */}
+          <div className='tw-relative'>
+            <div className='tw-px-5 tw-w-full tw-h-[356px]'>
               <Image src={TrophyIconGreen} width={52} height={52} alt="dollar" className='tw-w-[52px] tw-h-[52px] ' />
+              <div className='tw-font-bold tw-text-[18px]'>Team A</div>
+              <div className='tw-text-[14px]'>11 Players</div>
+              <div className='tw-relative tw-mt-4'>
+                {teamPlayers.map((player) => {
+                  return <div key={player.id} className='tw-mb-4 tw-flex'>
+                    <Image src={player.avatar} width={40} height={40} alt="dollar" className='tw-w-[40px] tw-h-[40px] tw-mr-4' />
+                    <div className='tw-text-sm '>
+                      <div className='tw-font-bold'>{player.amount}</div>
+                      <div>{player.username}</div>
+                    </div>
+                  </div>
+                })}
+              </div>
+              <button className='btn-green tw-absolute tw-bottom-[-20px] tw-right-[16px]'>Wager on Team B</button>
             </div>
+            {/* Background */}
             <div className='tw-absolute tw-top-[26px] tw-h-[362px] tw-z-[-1]'>
               <Image src={TransitionPattern} width={288} height={356} alt="pattern" className='tw-w-auto tw-h-[288px]  tw-rounded-lg tw-mr-1 tw-object-cover' />
               <div className='tw-w-full tw-h-full tw-rounded-lg tw-absolute tw-top-0 tw-bg-[#49C74233]'></div>
             </div>
           </div>
 
+          {/* Team B */}
           <div className='tw-relative'>
-            <div className='tw-px-5'>
+            <div className='tw-px-5 tw-w-full tw-h-[356px]'>
               <Image src={TrophyIconBlue} width={52} height={52} alt="dollar" className='tw-w-[52px] tw-h-[52px] ' />
-              <div className='tw-font-bold tw-text-[18px]'>Team A</div>
+              <div className='tw-font-bold tw-text-[18px]'>Team B</div>
               <div className='tw-text-[14px]'>11 Players</div>
-              <div>
-
+              <div className='tw-relative tw-mt-4'>
+                {teamPlayers.map((player) => {
+                  return <div key={player.id} className='tw-mb-4 tw-flex'>
+                    <Image src={player.avatar} width={40} height={40} alt="dollar" className='tw-w-[40px] tw-h-[40px] tw-mr-4' />
+                    <div className='tw-text-sm '>
+                      <div className='tw-font-bold'>{player.amount}</div>
+                      <div>{player.username}</div>
+                    </div>
+                  </div>
+                })}
               </div>
+              <button className='btn-blue tw-absolute tw-bottom-[-20px] tw-right-[16px]'>Wager on Team B</button>
             </div>
+            {/* Background */}
             <div className='tw-absolute tw-top-[26px] tw-h-[362px] tw-z-[-1]'>
               <Image src={TransitionPattern} width={288} height={356} alt="pattern" className='tw-w-auto tw-h-[288px]  tw-rounded-lg tw-mr-1 tw-object-cover' />
               <div className='tw-w-full tw-h-full tw-rounded-lg tw-absolute tw-top-0 tw-bg-[#156CC333]'></div>
