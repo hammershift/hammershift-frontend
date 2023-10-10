e61fe38405318129efed5dc2875408eecb936d8f
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-10-10 16:25:44 +0800

    fix: added dropdown menu

--- src/app/auction_listing_page/page.tsx
+++ src/app/auction_listing_page/page.tsx
@@ -1,5 +1,6 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import GiftIcon from '../../../public/images/gift-02.svg'
 import Image from 'next/image'
 
@@ -8,6 +9,7 @@ const AuctionListingPage = () => {
         <div className='tw-flex tw-flex-col tw-items-center'>
             <TopNavigation />
             <Filters />
+            <Dropdown />
         </div>
     )
 }
@@ -51,6 +53,41 @@ const Filters = () => {
                 <div>Dropdown</div>
             </div>
 
+        </div>
+
+    )
+}
+
+
+
+
+const Dropdown = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    return (
+        <div className="tw-relative tw-inline-block tw-text-left">
+            <div>
+                <button type="button" className="tw-w-[140px] tw-inline-flex tw-justify-between tw-gap-x-1.5 tw-rounded-md tw-px-3 tw-py-2 tw-text-sm tw-font-semibold tw-text-white-900 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-gray-300 tw-bg-[#1A2C3D] hover:tw-bg-black" id="menu-button" aria-expanded="true" aria-haspopup="true" onClick={() => setMenuOpen(prev => !prev)}>
+                    Options
+                    <svg className="tw--mr-1 tw-h-5 tw-w-5 tw-text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                    </svg>
+                </button>
+            </div>
+
+            {menuOpen &&
+
+                <div className="tw-absolute tw-left-0 tw-z-10 tw-mt-2 tw-w-56 tw-origin-top-right tw-rounded-md tw-bg-[#1A2C3D] tw-text-white tw-shadow-lg tw-ring-1 tw-ring-black tw-ring-opacity-5 focus:yw-outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex={-1}>
+                    <div className="tw-py-1" role="none">
+
+                        <a href="#" className="tw-text-white tw-block tw-px-4 tw-py-2 tw-text-sm" role="menuitem" tabIndex={-1} id="menu-item-0">Account settings</a>
+                        <a href="#" className="tw-text-white tw-block tw-px-4 tw-py-2 tw-text-sm" role="menuitem" tabIndex={-1} id="menu-item-1">Support</a>
+                        <a href="#" className="tw-text-white tw-block tw-px-4 tw-py-2 tw-text-sm" role="menuitem" tabIndex={-1} id="menu-item-2">License</a>
+                        <form method="POST" action="#" role="none">
+                            <button type="submit" className="tw-text-white tw-block tw-w-full tw-px-4 tw-py-2 tw-text-left tw-text-sm" role="menuitem" tabIndex={-1} id="menu-item-3">Sign out</button>
+                        </form>
+                    </div>
+                </div>
+            }
         </div>
 
     )
