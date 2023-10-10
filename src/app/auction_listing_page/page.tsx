cd9e14cba2949cdd27296aec28f8dbbedecfbae6
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-10-10 16:58:03 +0800

    fix: fixed bg color of dropdown when open

--- src/app/auction_listing_page/page.tsx
+++ src/app/auction_listing_page/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import GiftIcon from '../../../public/images/gift-02.svg'
 import Image from 'next/image'
+import DropdownArrow from '../../../public/images/dropdown.svg'
+import MagnifyingGlass from '../../../public/images/magnifying-glass.svg'
 
 const AuctionListingPage = () => {
     return (
@@ -63,22 +65,31 @@ const Filters = () => {
 
 const Dropdown = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [bgColor, setBgColor] = useState({});
+    useEffect(() => {
+        if (menuOpen === true) {
+            setBgColor({ backgroundColor: "#1A2C3D" })
+        } else {
+            setBgColor({})
+        }
+    }, [menuOpen])
     return (
         <div className="tw-relative tw-inline-block tw-text-left">
             <div>
-                <button type="button" className="tw-w-[140px] tw-inline-flex tw-justify-between tw-gap-x-1.5 tw-rounded-md tw-px-3 tw-py-2 tw-text-sm tw-font-semibold tw-text-white-900 tw-shadow-sm tw-ring-1 tw-ring-inset tw-ring-gray-300 tw-bg-[#1A2C3D] hover:tw-bg-black" id="menu-button" aria-expanded="true" aria-haspopup="true" onClick={() => setMenuOpen(prev => !prev)}>
-                    Options
-                    <svg className="tw--mr-1 tw-h-5 tw-w-5 tw-text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                    </svg>
+                <button type="button" className="tw-w-[140px] tw-inline-flex tw-justify-between tw-items-center tw-gap-x-1.5 tw-rounded-md tw-px-3 tw-py-2.5 tw-text-sm  tw-text-white-900 tw-shadow-sm tw-bg-[#172431] hover:tw-bg-[#1A2C3D]" id="menu-button" aria-expanded="true" aria-haspopup="true" style={bgColor} onClick={() => setMenuOpen(prev => !prev)}>
+                    Make
+                    <Image src={DropdownArrow} width={12} height={12} alt='dropdown arrow' className='tw-w-[12px] tw-h-[12px]' />
                 </button>
             </div>
 
             {menuOpen &&
 
-                <div className="tw-absolute tw-left-0 tw-z-10 tw-mt-2 tw-w-56 tw-origin-top-right tw-rounded-md tw-bg-[#1A2C3D] tw-text-white tw-shadow-lg tw-ring-1 tw-ring-black tw-ring-opacity-5 focus:yw-outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex={-1}>
-                    <div className="tw-py-1" role="none">
-
+                <div className="tw-absolute tw-left-0 tw-z-10 tw-mt-2 tw-w-[640px] tw-h-[362px] tw-origin-top-right tw-rounded-md tw-bg-[#1A2C3D] tw-text-white tw-shadow-lg " role="menu" aria-labelledby="menu-button" tabIndex={-1}>
+                    <div className="tw-p-4" role="none">
+                        <div className='tw-flex tw-items-center tw-bg-white/5 tw-rounded tw-py-2 tw-px-3'>
+                            <Image src={MagnifyingGlass} width={20} height={20} alt='dropdown arrow' className='tw-w-[20px] tw-h-[20px] tw-mr-2' />
+                            <input className='tw-bg-transparent tw-w-full' placeholder='Search' />
+                        </div>
                         <a href="#" className="tw-text-white tw-block tw-px-4 tw-py-2 tw-text-sm" role="menuitem" tabIndex={-1} id="menu-item-0">Account settings</a>
                         <a href="#" className="tw-text-white tw-block tw-px-4 tw-py-2 tw-text-sm" role="menuitem" tabIndex={-1} id="menu-item-1">Support</a>
                         <a href="#" className="tw-text-white tw-block tw-px-4 tw-py-2 tw-text-sm" role="menuitem" tabIndex={-1} id="menu-item-2">License</a>
