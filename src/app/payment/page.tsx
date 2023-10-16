2edf6ee2c0b335995249ba5dc909eb250a688ec1
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-10-16 16:43:21 +0800

    fix: created payment flow

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -8,6 +8,10 @@ import GooglePayLogo from '../../../public/images/payments-logo/google-pay.svg'
 import MasterCardLogo from '../../../public/images/payments-logo/Mastercard.svg'
 import PaypalLogo from '../../../public/images/payments-logo/paypal.svg'
 import VisaLogo from '../../../public/images/payments-logo/visa.svg'
+import CardIcon from '../../../public/images/payments-logo/card.svg'
+import HelpIcon from '../../../public/images/payments-logo/help-icon.svg'
+
+import CountryOptions from '../components/country_option'
 
 
 const Payment = () => {
@@ -68,7 +72,65 @@ const Payment = () => {
                 {
                     paymentChoice === "Credit Card"
                     &&
-                    <div>Hello</div>
+                    <div className='tw-bg-[#172431] tw-p-4 tw-rounded'>
+                        <div className=' tw-h-[60px] tw-px-4 tw-w-full tw-flex tw-items-center tw-justify-between  tw-rounded tw-mb-3'>
+                            <div className=''>Credit or Debit Card</div>
+                            <div className='tw-flex tw-grid tw-grid-cols-4 tw-gap-2'>
+                                <Image src={VisaLogo} width={52} height={36} alt='x' className='tw-w-[52px] tw-h-[36px]' />
+                                <Image src={MasterCardLogo} width={52} height={36} alt='x' className='tw-w-[52px] tw-h-[36px]' />
+                                <Image src={AmexLogo} width={52} height={36} alt='x' className='tw-w-[52px] tw-h-[36px]' />
+                                <Image src={DiscoverLogo} width={52} height={36} alt='x' className='tw-w-[52px] tw-h-[36px]' />
+                            </div>
+                        </div>
+                        <hr className='tw-border-white ' />
+                        {/* inputs */}
+                        <div className='tw-grid tw-gap-3 tw-py-6'>
+                            <div>
+                                <label>Card Number</label>
+                                <div className='tw-bg-white/5 tw-flex tw-h-11 tw-py-2.5 tw-px-3 tw-mt-2'>
+                                    <Image src={CardIcon} width={35} height={24} alt='x' className='tw-w-[35px] tw-h-[24px]' />
+                                    <input className='tw-bg-transparent tw-ml-2' placeholder='0000 0000 0000 0000' />
+                                </div>
+                            </div>
+                            <div className='tw-grid tw-grid-cols-2 tw-gap-4'>
+                                <div>
+                                    <label>Expiration</label>
+                                    <div className='tw-bg-white/5 tw-flex tw-items-center tw-h-11 tw-py-2.5 tw-px-3 tw-mt-2'>
+                                        <input className='tw-bg-transparent tw-ml-2' placeholder='MM/YY' />
+                                        <Image src={HelpIcon} width={20} height={20} alt='x' className='tw-w-[20px] tw-h-[20px]' />
+                                    </div>
+                                </div>
+                                <div>
+                                    <label>CVV</label>
+                                    <div className='tw-bg-white/5 tw-flex tw-items-center tw-h-11 tw-py-2.5 tw-px-3 tw-mt-2'>
+                                        <input className='tw-bg-transparent tw-ml-2' placeholder='123' />
+                                        <Image src={HelpIcon} width={20} height={20} alt='x' className='tw-w-[20px] tw-h-[20px]' />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='tw-grid tw-grid-cols-2 tw-gap-4 tw-bg-[#1018280D]'>
+                                <div>
+                                    <label>Country</label>
+                                    <div className='tw-bg-white/5 tw-flex tw-items-center tw-h-11 tw-py-2.5 tw-px-3 tw-mt-2'>
+                                        <CountryOptions />
+
+                                    </div>
+                                </div>
+                                <div>
+                                    <label>Zip Code</label>
+                                    <div className='tw-bg-white/5 tw-flex tw-items-center tw-h-11 tw-py-2.5 tw-px-3 tw-mt-2'>
+                                        <input className='tw-bg-transparent tw-ml-2 tw-w-full' />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr className='tw-border-white' />
+                        <div className='tw-py-4 tw-flex tw-justify-end'>
+                            <button className='btn-transparent-white'>CANCEL</button>
+                            <button className='btn-yellow tw-ml-4'>CONTINUE</button>
+                        </div>
+                    </div>
+
                 }
             </div>
 
