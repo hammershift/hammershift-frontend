5409797d4a0dc2a464748203a4379ea6a1ddd608
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-10-17 21:49:57 +0800

    fix: completed tournament wager page

--- src/app/tournament_wager_page/page.tsx
+++ src/app/tournament_wager_page/page.tsx
@@ -2,15 +2,48 @@ import React from 'react'
 import Image from 'next/image'
 import MoneyBag from '../../../public/images/money-bag-green.svg'
 import PlayersIcon from '../../../public/images/players-icon-green.svg'
+import HourGlass from '../../../public/images/hour-glass.svg'
+import Dollar from '../../../public/images/dollar.svg'
+import SedanPhotoOne from '../../../public/images/tournament-wager/sedan-photo-one.svg'
+import SedanPhotoTwo from '../../../public/images/tournament-wager/sedan-photo-two.svg'
+import SedanPhotoThree from '../../../public/images/tournament-wager/sedan-photo-three.svg'
+import SedanPhotoFour from '../../../public/images/tournament-wager/sedan-photo-four.svg'
+import SedanPhotoFive from '../../../public/images/tournament-wager/sedan-photo-five.svg'
 
 const TournamentWagerPage = () => {
+    const tournamentWagerPageData = [{
+        img: SedanPhotoOne,
+        name: "620-Mile 2019 Ford GT",
+        current_bid: "$904,000",
+        time_left: "12:17:00"
+    }, {
+        img: SedanPhotoTwo,
+        name: "620-Mile 2019 Ford GT",
+        current_bid: "$904,000",
+        time_left: "12:17:00"
+    }, {
+        img: SedanPhotoThree,
+        name: "620-Mile 2019 Ford GT",
+        current_bid: "$904,000",
+        time_left: "12:17:00"
+    }, {
+        img: SedanPhotoFour,
+        name: "620-Mile 2019 Ford GT",
+        current_bid: "$904,000",
+        time_left: "12:17:00"
+    }, {
+        img: SedanPhotoFive,
+        name: "620-Mile 2019 Ford GT",
+        current_bid: "$904,000",
+        time_left: "12:17:00"
+    }]
     return (
         <div className='tw-bg-black/50 tw-w-screen tw-h-screen tw-flex tw-justify-center tw-items-center tw-absolute tw-top-0 tw-left-0'>
             <div className='tw-relative tw-bg-[#0F1923] tw-w-[864px] tw-h-[900px] tw-py-8 tw-px-6'>
-                <div>
-                    <div>Sedan Champions Tournament</div>
+                <div className='tw-flex tw-flex-col tw-gap-4'>
+                    <div className='tw-text-4xl tw-font-bold'>Sedan Champions Tournament</div>
                     <div>Get more points the closer you are to the hammer price of a curated set of car auctions. Guess the price for each of the cars listed below and buy-in to lock in your wagers. <span>Learn more</span></div>
-                    <div className='tw-text-[#49C742] tw-text-lg tw-mt-6 tw-py-3 tw-px-4 tw-bg-[#49C74233] tw-grid tw-grid-cols-2'>
+                    <div className='tw-text-[#49C742] tw-text-lg tw-py-3 tw-px-4 tw-bg-[#49C74233] tw-grid tw-grid-cols-2'>
                         <div className='tw-flex tw-items-center tw-gap-2'>
                             <Image src={MoneyBag} width={32} height={32} alt='money bag' className='tw-w-[32px] tw-h-[32px]' />
                             <div className=''>
@@ -26,6 +59,16 @@ const TournamentWagerPage = () => {
                             </div>
                         </div>
                     </div>
+                    <div className='tw-flex tw-flex-col tw-gap-4'>
+                        {
+                            tournamentWagerPageData.map((data) =>
+                                <div key={data.img}>
+                                    <TournamentWagerPageCard img={data.img} name={data.name} current_bid={data.current_bid} time_left={data.time_left} />
+
+                                </div>
+                            )
+                        }
+                    </div>
                 </div>
             </div>
 
@@ -34,3 +77,42 @@ const TournamentWagerPage = () => {
 }
 
 export default TournamentWagerPage
+
+
+interface TournamentWagerPageCardProps {
+    img: string
+    name: string
+    current_bid: string
+    time_left: string
+}
+
+const TournamentWagerPageCard: React.FC<TournamentWagerPageCardProps> = ({ img, name, current_bid, time_left }) => {
+    return (
+        <div className='tw-text-sm tw-flex tw-justify-between tw-items-center'>
+            <div className='tw-flex tw-gap-4'>
+                <Image src={img} width={100} height={100} alt='sedan' className='tw-w-[100px] tw-h-[100px]' />
+                <div className='tw-flex tw-flex-col tw-justify-center'>
+                    <div className='tw-text-lg tw-font-bold'>{name}</div>
+                    <div className='tw-flex tw-gap-2 tw-items-center'>
+                        <Image src={Dollar} width={14} height={14} alt='sedan' className='tw-w-[14px] tw-h-[14px]' />
+                        <span className='tw-opacity-80'>Current Bid:</span>
+                        <span className='tw-font-bold tw-text-[#49C742]'>{current_bid}</span>
+                    </div>
+                    <div className='tw-flex tw-gap-2 tw-items-center'>
+                        <Image src={HourGlass} width={14} height={14} alt='sedan' className='tw-w-[14px] tw-h-[14px]' />
+                        <span className='tw-opacity-80'>Time Left:</span>
+                        <span>{current_bid}</span>
+                    </div>
+                </div>
+            </div>
+            <div className='tw-relative tw-flex tw-items-center tw-rounded tw-w-[224px]'>
+                <div className='tw-w-lg tw-h-auto tw-top-[50%] tw--translate-y-[50%] tw-left-3 tw-absolute tw-text-gray-500 tw-z-20' >$</div>
+                <input className='tw-bg-white/5 tw-py-3 tw-pl-8 tw-pr-3 tw-w-full focus:tw-bg-white focus:tw-text-black focus:tw-border-2 focus:tw-border-white/10 ' />
+            </div>
+            <div className='tw-absolute tw-items-center tw-flex tw-justify-between tw-bottom-0 tw-left-0 tw-h-[80px] tw-w-full tw-p-8 tw-bg-[#172431]'>
+                <button>CANCEL</button>
+                <button className='btn-yellow tw-h-[48px] tw-w-[224px]'>BUY-IN FOR $100</button>
+            </div>
+        </div>
+    )
+}
