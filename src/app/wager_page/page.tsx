a5eac4dd62a3f529a5cb27732be8ae417e3ef260
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-10-18 14:01:53 +0800

    fix: finished responsive design of wager page

--- src/app/wager_page/page.tsx
+++ src/app/wager_page/page.tsx
@@ -11,10 +11,11 @@ import GoogleSocial from '../../../public/images/social-google-logo.svg'
 import FacebookSocial from '../../../public/images/social-facebook-logo.svg'
 import TwitterSocial from '../../../public/images/social-twitter-logo.svg'
 import AppleSocial from '../../../public/images/social-apple-logo.svg'
+import CancelIcon from '../../../public/images/x-icon.svg'
 
 const WagerPage = () => {
     const user = {
-        isregistered: false
+        isregistered: true
     }
 
     const WagerPageData = {
@@ -30,28 +31,32 @@ const WagerPage = () => {
             {/* Content */}
             {user.isregistered
                 ?
-                <div className='tw-relative tw-bg-[#0F1923] tw-w-[864px] tw-h-[900px] tw-py-8 tw-px-6'>
-                    <div className='tw-flex tw-flex-col md:tw-flex-row'>
-                        <Image src={CarPhoto} width={360} height={173} alt='fray car' className='tw-w-full md:tw-w-[136px] tw-h-auto md:tw-h-[136px] tw-object-cover' />
-                        <div className='md:tw-ml-6 tw-mt-6 md:tw-mt-0 tw-text-3xl'>
+                <div className='tw-relative tw-bg-[#0F1923] tw-w-[864px] tw-h-auto md:tw-h-[900px] tw-pt-8 tw-flex tw-flex-col tw-gap-6'>
+                    <div className='tw-flex tw-flex-col md:tw-flex-row tw-gap-6 tw-px-6'>
+                        <div className='tw-flex md:tw-hidden tw-items-center tw-justify-between  md:tw-justify-start tw-w-full'>
+                            <div className='tw-text-2xl sm:tw-text-4xl tw-font-bold'>Guess the Price</div>
+                            <Image src={CancelIcon} width={20} height={20} alt='' className='tw-w-[20px] tw-h-[20px] ' />
+                        </div>
+                        <Image src={CarPhoto} width={360} height={173} alt='fray car' className='tw-w-full md:tw-w-[136px] tw-h-auto md:tw-h-[136px] tw-object-cover tw-aspect-3/4 md:tw-aspect-auto' />
+                        <div className='md:tw-ml-6 md:tw-mt-0 tw-text-3xl'>
                             <div className='tw-font-bold'>{WagerPageData.name}</div>
                             <div className='tw-grid tw-gap-2 tw-mt-4'>
                                 <div className='tw-grid tw-grids-cols-1 md:tw-grid-cols-2 tw-text-sm tw-gap-2'>
                                     <div className='tw-flex tw-items-center'>
                                         <Image src={DollarIcon} width={14} height={14} alt='' className='tw-w-[14px] tw-h-[14px]' />
-                                        <div className='tw-text-sm tw-ml-2'>
+                                        <div className='tw-text-sm tw-ml-2 tw-flex tw-flex-row tw-gap-2'>
                                             <span className='tw-opacity-80'>Current Bid:</span>
-                                            <span className='tw-text-[#49C742] tw-font-bold tw-ml-2'>{WagerPageData.current_bid}</span>
+                                            <span className='tw-text-[#49C742] tw-font-bold'>{WagerPageData.current_bid}</span>
+                                            <span className='md:tw-hidden'>{`(${WagerPageData.num_bids} bids)`}</span>
                                         </div>
                                     </div>
-                                    <div className='tw-flex tw-items-center'>
+                                    <div className='tw-hidden md:tw-flex tw-items-center '>
                                         <Image src={HashtagIcon} width={14} height={14} alt='' className='tw-w-[14px] tw-h-[14px]' />
                                         <div className='tw-text-sm tw-ml-2'>
                                             <span className='tw-opacity-80'>Bids:</span>
                                             <span className=' tw-font-bold tw-ml-2'>{WagerPageData.num_bids}</span>
                                         </div>
                                     </div>
-
                                 </div>
                                 <div className='tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-2 tw-text-sm'>
                                     <div className='tw-flex tw-items-center'>
@@ -69,26 +74,25 @@ const WagerPage = () => {
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
                     </div>
-                    <hr className='tw-border-white tw-my-8' />
-                    <div className=' tw-flex tw-flex-col tw-gap-2'>
+                    <hr className='tw-border-white' />
+                    <div className=' tw-flex tw-flex-col tw-gap-3 tw-px-6'>
                         <label className='tw-text-lg'>How much will this sell for?</label>
                         <div className='tw-relative tw-flex tw-items-center tw-rounded '>
                             <div className='tw-w-lg tw-h-auto tw-top-[50%] tw--translate-y-[50%] tw-left-3 tw-absolute tw-text-gray-500 tw-z-20' >$</div>
                             <input className='tw-bg-white/5 tw-py-3 tw-pl-8 tw-pr-3 tw-w-full focus:tw-bg-white focus:tw-text-black focus:tw-border-2 focus:tw-border-white/10 ' />
                         </div>
                     </div>
-                    <div className='tw-mt-6 tw-flex tw-flex-col tw-gap-2'>
+                    <div className=' tw-flex tw-flex-col tw-gap-3 tw-px-6'>
                         <label className='tw-text-lg'>Wager</label>
                         <div className='tw-relative tw-flex tw-items-center tw-rounded '>
                             <div className='tw-w-lg tw-h-auto tw-top-[50%] tw--translate-y-[50%] tw-left-3 tw-absolute tw-text-gray-500 tw-z-20' >$</div>
                             <input className='tw-bg-white/5 tw-py-3 tw-pl-8 tw-pr-3 tw-w-full focus:tw-bg-white focus:tw-text-black focus:tw-border-2 focus:tw-border-white/10 ' />
                         </div>
                     </div>
-                    <div className='tw-text-[#49C742] tw-text-lg tw-mt-6 tw-py-3 tw-px-4 tw-bg-[#49C74233] tw-grid tw-grid-cols-2'>
+                    <div className='tw-text-[#49C742] tw-text-lg  tw-py-3 tw-px-4 tw-bg-[#49C74233] tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-4 tw-mx-6 tw-px-6 tw-rounded'>
                         <div className='tw-flex tw-items-center tw-gap-2'>
                             <Image src={MoneyBag} width={32} height={32} alt='money bag' className='tw-w-[32px] tw-h-[32px]' />
                             <div className=''>
@@ -104,9 +108,9 @@ const WagerPage = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='tw-absolute tw-items-center tw-flex tw-justify-between tw-bottom-0 tw-left-0 tw-h-[80px] tw-w-full tw-p-8 tw-bg-white/5'>
-                        <button>CANCEL</button>
-                        <button className='btn-yellow tw-h-[48px]'>PLACE MY WAGER</button>
+                    <div className='md:tw-absolute md:tw-bottom-0 md:tw-left-0 tw-items-center tw-flex tw-justify-between tw-h-[80px] tw-w-full tw-p-6 tw-bg-white/5'>
+                        <button className='tw-hidden md:tw-block'>CANCEL</button>
+                        <button className='btn-yellow tw-h-[48px] tw-w-full md:tw-w-auto'>PLACE MY WAGER</button>
                     </div>
 
                 </div>
