a6300423738a467f5d3af12c16e4979d422669dc
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-11-17 08:24:27 +0800

    fix: created get route that finds auction using auction id

--- src/app/api/cars/route.ts
+++ src/app/api/cars/route.ts
@@ -3,18 +3,28 @@ import { NextResponse, NextRequest } from 'next/server'
 import Car from "@/app/models/car"
 import connectMongoDB from '@/app/lib/mongodb'
 
-type Props = {
-    params: {
-        id: string
-    }
-}
 
-// get one document using auction_id
+// get document using auction_id : http://localhost:3000/api/cars?id=63075635 : http://localhost:3000/api/cars?limit=21&offset=0
 export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url);
     const id = searchParams.get("id");
-
+    const limit = searchParams.get("limit");
+    const offset = searchParams.get("offset");
     await connectMongoDB();
-    const cars = await Car.findOne({ auction_id: id });
-    return NextResponse.json({ cars });
+    if (id) {
+        try {
+            const cars = await Car.findOne({ auction_id: id });
+            return NextResponse.json({ cars });
+
+        } catch (error) {
+            console.error({ error: error });
+        }
+
+    } else {
+        //TODO: add status to properties
+        // const cars = await Car.find(status : "ongoing");
+        // const carsArray = cars.slice(offset, limit)
+        // return NextResponse.json({ cars });
+    }
 }
+
