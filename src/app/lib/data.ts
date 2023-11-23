21ab5e98e92c06fbd4ce32d784c157c5029c4405
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-11-23 09:45:30 +0800

    fix: added cache: no store

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -2,9 +2,12 @@
 export function getCarData(ID: string) {
     //DOMAIN=https://hammershift-git-feat-create-api-hammershifts-projects.vercel.app/
     const URL = process.env.DOMAIN || "http://localhost:3000/"
-    return fetch(URL + `api/cars?auction_id=${ID}`)
+    return fetch(URL + `api/cars?auction_id=${ID}`, {
+        cache: 'no-store' //dynamic rendering
+    })
         .then((res) => res.json())
         .then(data => {
+            console.log(data)
             return data;
         })
         .catch((error) => {
