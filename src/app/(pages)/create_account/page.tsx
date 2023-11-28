88af8ac30305f5d8c16088c36719afe92d81549a
Amiel Brencis Salipande <absalipande@gmail.com>
2023-11-28 15:17:05 +0800

    feat: Implement dynamic country-state selection and Google provider in create_account

--- src/app/(pages)/create_account/page.tsx
+++ src/app/(pages)/create_account/page.tsx
@@ -1,5 +1,6 @@
 'use client';
-import React, { useState } from 'react';
+
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -13,46 +14,145 @@ import SingleNeutral from '../../../../public/images/single-neutral-id-card-3.sv
 import UserImage from '../../../../public/images/user-single-neutral-male--close-geometric-human-person-single-up-user-male.svg';
 import Passport from '../../../../public/images/passport.svg';
 import IDCard from '../../../../public/images/single-neutral-id-card-1.svg';
-import { signIn } from 'next-auth/react';
+
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { ICountry, IState, Country, State } from 'country-state-city';
 
 const CreateAccount = () => {
   type createAccountPageProps = 'page one' | 'page two' | 'page three';
   const [createAccountPage, setCreateAccountPage] = useState<createAccountPageProps>('page one');
 
-  // TEST IMPLEMENTATION
+  // TEST
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
   const [fullName, setFullName] = useState('');
   const [username, setUsername] = useState('');
   const [country, setCountry] = useState('');
   const [state, setState] = useState('');
   const [aboutMe, setAboutMe] = useState('');
 
-  // creating an account handler (page one)
-  const handleSignIn = async () => {
-    try {
-      console.log('Attempting to sign in with:', { email });
-      const result = await signIn('credentials', {
-        redirect: false,
-        email: email,
-        password: password,
-      });
+  // for country and state selection
+  const [countries, setCountries] = useState<ICountry[]>([]);
+  const [states, setStates] = useState<IState[]>([]);
+  const [selectedCountry, setSelectedCountry] = useState<ICountry | null>(null);
+  const [selectedState, setSelectedState] = useState<IState | null>(null);
 
-      console.log('signIn result:', result);
+  // session and routing
+  const { data: session } = useSession();
+  const router = useRouter();
 
-      if (result?.error) {
-        console.error(result.error);
-      } else {
-        setCreateAccountPage('page two');
-      }
-    } catch (error) {
-      console.error('Error during account creation:', error);
+  // load all countries for the dropdown
+  useEffect(() => {
+    setCountries(Country.getAllCountries());
+  }, []);
+
+  // navigate to page two if session exists
+  useEffect(() => {
+    if (session) {
+      setCreateAccountPage('page two');
+    }
+  }, [session]);
+
+  // TEST IMPLEMENTATION FOR COUNTRY AND STATE
+  const handleCountrySelect = (countryCode: string) => {
+    const country = Country.getCountryByCode(countryCode);
+    if (country) {
+      setSelectedCountry(country);
+      setStates(State.getStatesOfCountry(country.isoCode));
+      setCountry(country.name);
     }
   };
 
-  // another handler for setting up the profile (page two)
-  const handleSetupProfile = async () => {
-    // TODO: ADD API
+  const handleStateSelect = (stateCode: string) => {
+    if (selectedCountry) {
+      const state = State.getStateByCodeAndCountry(stateCode, selectedCountry.isoCode);
+      if (state) {
+        setSelectedState(state);
+        setState(state.name);
+      }
+    }
+  };
+
+  // handler for account creation logic
+  const handleAccountCreation = async () => {
+    const response = await fetch('/api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      console.log('Registration successful:', data.message);
+      const signInResponse = await signIn('credentials', {
+        redirect: false,
+        email,
+        password,
+      });
+
+      if (signInResponse?.error) {
+        console.error('Sign-in failed:', signInResponse.error);
+      } else {
+        setCreateAccountPage('page two');
+      }
+    } else {
+      console.error('Registration failed:', data.message);
+    }
+  };
+
+  // handler for profile submission logic
+  const handleProfileSubmission = async () => {
+    const profileData = { fullName, username, country: selectedCountry?.name, state: selectedState?.name, aboutMe };
+    console.log('Profile Data being sent:', profileData);
+
+    const response = await fetch('/api/userInfo', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(profileData),
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      console.log('Profile updated successfully:', data.message);
+      //   setCreateAccountPage('page three');
+      handleVerifyLater();
+    } else {
+      console.error(data.message);
+    }
+  };
+
+  // for Google signin
+  const handleGoogleSignIn = async (provider: string) => {
+    try {
+      await signIn(provider, { callbackUrl: window.location.href });
+    } catch (error) {
+      console.error(`Error during ${provider} sign in:`, error);
+    }
+  };
+
+  // handler for skipping the verification of credentials with onfido
+  const handleVerifyLater = async () => {
+    const userData = { fullName, username, country, state, aboutMe };
+    const response = await fetch('/api/userInfo', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      router.push('/');
+    } else {
+      console.error('Failed to update profile:', data.message);
+    }
   };
 
   return (
@@ -74,23 +174,18 @@ const CreateAccount = () => {
           <div className='tw-flex tw-flex-col tw-gap-6 tw-text-sm'>
             <div className='tw-flex tw-flex-col tw-gap-2'>
               <label>Email</label>
-              <input className='tw-py-2.5 tw-px-3 tw-bg-[#172431]' placeholder='you@email.com' />
+              <input className='tw-py-2.5 tw-px-3 tw-bg-[#172431]' placeholder='you@email.com' value={email} onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div className='tw-flex tw-flex-col tw-gap-2'>
               <label>Password</label>
-              <input className='tw-py-2.5 tw-px-3 tw-bg-[#172431]' />
+              <input className='tw-py-2.5 tw-px-3 tw-bg-[#172431]' value={password} onChange={(e) => setPassword(e.target.value)} />
             </div>
-            <button
-              className='btn-yellow'
-              onClick={() => {
-                setCreateAccountPage('page two');
-              }}
-            >
+            <button className='btn-yellow' onClick={handleAccountCreation}>
               CREATE ACCOUNT
             </button>
           </div>
-          <div className='tw-w-full tw-grid tw-grid-cols-4 tw-gap-2'>
-            <div className='tw-bg-white tw-flex tw-justify-center tw-items-center tw-rounded tw-h-[48px]'>
+          <div className='tw-w-full tw-grid tw-grid-cols-4 tw-gap-2 clickable-icon'>
+            <div onClick={() => handleGoogleSignIn('google')} className='tw-bg-white tw-flex tw-justify-center tw-items-center tw-rounded tw-h-[48px]'>
               <Image src={GoogleSocial} width={24} height={24} alt='google logo' className='tw-w-6 tw-h-6' />
             </div>
             <div className='tw-bg-[#1877F2] tw-flex tw-justify-center tw-items-center tw-rounded tw-h-[48px]'>
@@ -117,39 +212,71 @@ const CreateAccount = () => {
               </div>
               <div className='tw-flex tw-flex-col tw-justify-center tw-gap-2 tw-grow'>
                 <label>Full Name *</label>
-                <input className='tw-py-2.5 tw-px-3 tw-bg-[#172431]' placeholder='full name' />
+                <input className='tw-py-2.5 tw-px-3 tw-bg-[#172431]' placeholder='full name' value={fullName} onChange={(e) => setFullName(e.target.value)} />
               </div>
             </div>
             <div className='tw-flex tw-flex-col tw-justify-center tw-gap-2 tw-grow'>
               <label>Username *</label>
-              <input className='tw-py-2.5 tw-px-3 tw-bg-[#172431]' />
+              <input className='tw-py-2.5 tw-px-3 tw-bg-[#172431]' value={username} onChange={(e) => setUsername(e.target.value)} />
               <div className='tw-text-sm tw-opacity-40'>At least x characters with no special symbols</div>
             </div>
             <div className='tw-grid tw-grid-cols-2 tw-gap-5'>
               <div className='tw-flex tw-flex-col tw-justify-center tw-gap-2 tw-grow'>
+                {/* TEST IMPLEMENTATION */}
                 <label>Country *</label>
-                <input className='tw-py-2.5 tw-px-3 tw-bg-[#172431]' />
+                <select className='tw-py-2.5 tw-px-3 tw-bg-[#172431]' onChange={(e) => handleCountrySelect(e.target.value)} value={selectedCountry?.isoCode || ''}>
+                  <option value=''>Select Country</option>
+                  {countries.map((country) => (
+                    <option key={country.isoCode} value={country.isoCode}>
+                      {country.name}
+                    </option>
+                  ))}
+                </select>
+                {/* <input className='tw-py-2.5 tw-px-3 tw-bg-[#172431]' value={country} onChange={(e) => setCountry(e.target.value)} /> */}
               </div>
               <div className='tw-flex tw-flex-col tw-justify-center tw-gap-2 tw-grow'>
+                {/* TEST IMPLEMENTATION */}
                 <label>State *</label>
-                <input className='tw-py-2.5 tw-px-3 tw-bg-[#172431]' />
+                <select
+                  className='tw-py-2.5 tw-px-3 tw-bg-[#172431]'
+                  onChange={(e) => handleStateSelect(e.target.value)}
+                  value={selectedState?.isoCode || ''}
+                  disabled={!selectedCountry}
+                >
+                  <option value=''>Select State</option>
+                  {states.map((state) => (
+                    <option key={state.isoCode} value={state.isoCode}>
+                      {state.name}
+                    </option>
+                  ))}
+                </select>
+                {/* <input className='tw-py-2.5 tw-px-3 tw-bg-[#172431]' value={state} onChange={(e) => setState(e.target.value)} /> */}
               </div>
             </div>
             <div className='tw-flex tw-flex-col tw-justify-center tw-gap-2 tw-grow'>
               <label>About Me</label>
-              <textarea className='tw-py-2.5 tw-px-3 tw-bg-[#172431]' placeholder='Tell the community about yourself' rows={8} />
+              <textarea
+                className='tw-py-2.5 tw-px-3 tw-bg-[#172431]'
+                placeholder='Tell the community about yourself'
+                rows={8}
+                value={aboutMe}
+                onChange={(e) => setAboutMe(e.target.value)}
+              />
             </div>
             <div className='tw-flex tw-flex-col tw-gap-2'>
-              <button className='btn-yellow' onClick={() => setCreateAccountPage('page three')}>
+              <button className='btn-yellow' onClick={handleProfileSubmission}>
                 Proceed to Account Verification
               </button>
-              <button className='btn-transparent-yellow'>Verify Later</button>
+              <button className='btn-transparent-yellow' onClick={handleVerifyLater}>
+                Verify Later
+              </button>
             </div>
           </div>
         </div>
       )}
       {createAccountPage === 'page three' && (
         //Account Verification
+        // TODO: Integrate Onfido for user verification process
         <div className='tw-w-screen md:tw-w-[640px] tw-px-6 tw-flex tw-flex-col tw-gap-8 tw-pt-6'>
           <div>
             <div className='tw-font-bold tw-text-4xl sm:tw-text-[44px] tw-py-1'>Select document to upload</div>
