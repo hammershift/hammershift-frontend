ea6874abcb297848ac33e4d4fc762d83aada44a2
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-11-30 14:38:40 +0800

    fix: changed car model

--- src/models/car.model.ts
+++ src/models/car.model.ts
@@ -2,47 +2,48 @@ import mongoose from "mongoose";
 
 const carSchema = new mongoose.Schema(
   {
-    "auction_id": { "type": "string" },
-    "attributes": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "key": { "type": "string" },
-          "value": {
-            "anyOf": [
-              { "type": "string" },
-              { "type": "integer" },
-              { "type": "boolean" },
-              { "type": "number" },
-              { "type": "null" }
+    auction_id: { type: "string" },
+    attributes: {
+      type: "array",
+      items: {
+        type: "object",
+        properties: {
+          key: { type: "string" },
+          value: {
+            anyOf: [
+              { type: "string" },
+              { type: "integer" },
+              { type: "boolean" },
+              { type: "number" },
+              { type: "null" }
             ]
           }
         }
       }
     },
-    "createdAt": { "type": "string", "format": "date-time" },
-    "description": {
-      "type": "array",
-      "items": { "type": "string" }
+    createdAt: { type: "string", format: "date-time" },
+    description: {
+      type: "array",
+      items: { type: "string" }
     },
-    "images_list": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "placing": { "type": "integer" },
-          "src": { "type": "string" }
+    image: { type: "string" },
+    images_list: {
+      type: "array",
+      items: {
+        type: "object",
+        properties: {
+          placing: { type: "integer" },
+          src: { type: "string" }
         }
       }
     },
-    "listing_details": {
-      "type": "array",
-      "items": { "type": "string" }
+    listing_details: {
+      type: "array",
+      items: { type: "string" }
     },
-    "page_url": { "type": "string" },
-    "updatedAt": { "type": "string", "format": "date-time" },
-    "website": { "type": "string" }
+    page_url: { type: "string" },
+    updatedAt: { type: "string", format: "date-time" },
+    website: { type: "string" }
   }
 );
 
