291271e8fd5586b41c3804492a54eb06390ac4e0
Amiel Brencis Salipande <absalipande@gmail.com>
2023-12-01 16:43:17 +0800

    feat(/api/forgotPassword): Added an otp-generator package

--- src/app/api/forgotPassword/route.ts
+++ src/app/api/forgotPassword/route.ts
@@ -1,5 +1,6 @@
 import clientPromise from '@/app/lib/mongodb';
 import { NextRequest, NextResponse } from 'next/server';
+import otpGenerator from 'otp-generator';
 
 export async function POST(req: NextRequest) {
   try {
@@ -13,11 +14,29 @@ export async function POST(req: NextRequest) {
     // look for the user by email
     const user = await db.collection('users').findOne({ email });
     if (!user) {
-      // send a generic response
       return NextResponse.json({ message: 'If an email address is associated with an account, we will process the password reset.' }, { status: 200 });
     }
 
-    // TODO: send a reset token and expiration date for TESTING
+    // otp generator
+    const otp = otpGenerator.generate(6, {
+      digits: true,
+      specialChars: false,
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+    });
+    const expirationDate = new Date(new Date().getTime() + 5 * 6000);
+
+    // store it in the db
+    console.log('Inserting OTP into the database');
+    await db.collection('password_reset_tokens').insertOne({
+      userId: user._id,
+      email: email,
+      otp: otp,
+      expires: expirationDate,
+    });
+    console.log('OTP inserted successfully');
+
+    // TODO: Reset token to store, and expiration date maybe?
     return NextResponse.json({ message: 'Password reset process has been initiated' }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: 'An error occurred while processing the password reset request' }, { status: 500 });
