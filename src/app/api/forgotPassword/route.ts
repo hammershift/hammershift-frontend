d5a2d8df9a0ea8975b0e03e8d0675ac1c0f44d8f
Amiel Brencis Salipande <absalipande@gmail.com>
2023-12-01 20:05:32 +0800

    Reorganized folder structure

--- src/app/api/forgotPassword/route.ts
+++ src/app/api/forgotPassword/route.ts
@@ -1,4 +1,4 @@
-import clientPromise from '@/app/lib/mongodb';
+import clientPromise from '@/lib/mongodb';
 import { NextRequest, NextResponse } from 'next/server';
 import otpGenerator from 'otp-generator';
 
