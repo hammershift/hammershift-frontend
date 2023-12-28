3699124d99a31c2ed18cee594982f1a1be0c9e5b
rrmgarcia <ronaldravengarcia@gmail.com>
2023-12-28 10:10:58 +0800

    implemented lazy loading to cards

---
+++ src/app/components/beta_testing.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/app.css";
+
+const BetaTesting = () => {
+  return (
+    <div className="tw-sticky -tw-top-2 tw-z-40 tw-bg-yellow-400 tw-text-center tw-rounded-sm tw-p-2 tw-pt-4 tw-text-xs tw- tw-w-40 tw-text-slate-950 tw-ml-40 tw-font-bold clip-path">
+      Beta Testing in Progress
+    </div>
+  );
+};
+
+export default BetaTesting;
