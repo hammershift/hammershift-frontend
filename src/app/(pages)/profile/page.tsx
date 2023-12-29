bb0a155f30e6c0aeaece5cb4a2c37da94abd4938
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2023-12-29 10:17:50 +0800

    fix: added loading to wagers and watchlist when fetching data

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -15,31 +15,35 @@ import { getMyWagers, getMyWatchlist } from "@/lib/data";
 import { TimerProvider } from "@/app/_context/TimerContext";
 import { MyWagersCard, MyWatchlistCard } from "@/app/components/navbar";
 import { useRouter } from "next/navigation";
+import { MoonLoader, PulseLoader } from "react-spinners";
 
 interface Props { }
 
 function Profile(props: Props) {
     const [name, setName] = React.useState("User")
+    const [dataIsLoading, setDataIsLoading] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [activeWagers, setActiveWagers] = useState([]);
     const [completedWagers, setCompletedWagers] = useState([]);
     const [activeWatchlist, setActiveWatchlist] = useState([]);
     const [completedWatchlist, setCompletedWatchlist] = useState([]);
+    const [isActiveWager, setIsActiveWager] = useState(true);
     const { } = props;
     const { data } = useSession();
     const router = useRouter();
 
     useEffect(() => {
         if (data) setName(data?.user.name);
-        console.log(data);
-        console.log(data?.user.name);
+        // console.log(data);
+        // console.log(data?.user.name);
     }, [name, data])
 
     // fetch wagers
     useEffect(() => {
+        setDataIsLoading(true);
         const fetchWagers = async () => {
             const data = await getMyWagers();
-            console.log(data);
+            // console.log(data);
             const currentDate = new Date();
 
             if (!data.wagers || data.wagers.length !== 0) {
@@ -54,11 +58,17 @@ function Profile(props: Props) {
 
                 setActiveWagers(active);
                 setCompletedWagers(completed);
+                setDataIsLoading(false);
             }
             setIsLoading(false);
         };
         fetchWagers();
     }, []);
+
+
+    useEffect(() => {
+        console.log("active:", activeWagers, "completed:", completedWagers)
+    }, [activeWagers, completedWagers])
 
     // fetch watchlist
     useEffect(() => {
@@ -227,22 +237,58 @@ function Profile(props: Props) {
                     <div className="tw-text-lg tw-font-bold tw-text-[#f2ca16] tw-leading-7">
                         WAGERS
                     </div>
+                    <div className='tw-flex'>
+                        <button
+                            id='active-watchlist-button'
+                            onClick={() => setIsActiveWager(true)}
+                            className={`tw-border-b-2 tw-w-1/2 tw-py-2 tw-border-[#314150] tw-flex tw-justify-center tw-items-center tw-gap-2 ${isActiveWager == true ? 'tw-font-bold tw-text-lg tw-border-white' : ''}`}
+                        >
+                            <div>ACTIVE </div>
+                            {!isLoading && <span className='tw-px-1 tw-text-sm tw-bg-[#f2ca16] tw-rounded tw-font-bold tw-text-[#0f1923]'>{activeWagers.length}</span>}
+                        </button>
+                        <button
+                            id='completed-watchlist-button'
+                            onClick={() => setIsActiveWager(false)}
+                            className={`tw-border-b-2 tw-w-1/2 tw-py-2 tw-border-[#314150] ${isActiveWager == false ? 'tw-font-bold tw-text-lg tw-border-white' : ''}`}
+                        >
+                            COMPLETED
+                        </button>
+                    </div>
                     <div>
-                        {activeWagers.map((wager: any) => (
-                            <div key={wager._id}>
-                                <TimerProvider deadline={wager.auctionDeadline}>
-                                    <MyWagersCard
-                                        title={`${wager.auctionYear} ${wager.auctionMake} ${wager.auctionModel}`}
-                                        img={wager.auctionImage}
-                                        my_wager={wager.priceGuessed}
-                                        current_bid={wager.auctionPrice}
-                                        time_left={wager.auctionDeadline}
-                                        potential_prize={wager.auctionPot}
-                                        id={wager.auctionIdentifierId}
-                                    />
-                                </TimerProvider>
-                            </div>
-                        ))}
+                        {
+                            dataIsLoading
+                                ? <div className="tw-w-full tw-h-[100px] tw-flex tw-justify-center tw-items-center">
+                                    <PulseLoader color='#f2ca16' />
+                                </div>
+                                : isActiveWager == true
+                                    ? activeWagers.map((wager: any) => (
+                                        <div key={wager._id}>
+                                            <TimerProvider deadline={wager.auctionDeadline}>
+                                                <MyWagersCard
+                                                    title={`${wager.auctionYear} ${wager.auctionMake} ${wager.auctionModel}`}
+                                                    img={wager.auctionImage}
+                                                    my_wager={wager.priceGuessed}
+                                                    current_bid={wager.auctionPrice}
+                                                    time_left={wager.auctionDeadline}
+                                                    potential_prize={wager.auctionPot}
+                                                    id={wager.auctionIdentifierId}
+                                                />
+                                            </TimerProvider>
+                                        </div>
+                                    ))
+                                    : completedWagers.map((wager: any) => (
+                                        <div key={wager._id}>
+                                            <TimerProvider deadline={wager.auctionDeadline}>
+                                                <CompletedWagerCard
+                                                    title={`${wager.auctionYear} ${wager.auctionMake} ${wager.auctionModel}`}
+                                                    img={wager.auctionImage}
+                                                    my_wager={wager.priceGuessed}
+                                                    id={wager.auctionIdentifierId}
+                                                />
+                                            </TimerProvider>
+                                        </div>
+                                    ))
+                        }
                     </div>
                     {/* <UserWagerList /> */}
                 </div>
@@ -251,19 +297,23 @@ function Profile(props: Props) {
                         WATCHLIST
                     </div>
                     <div>
-                        {activeWatchlist.map((watchlist: any) => (
-                            <div key={watchlist._id}>
-                                <TimerProvider deadline={watchlist.auctionDeadline}>
-                                    <MyWatchlistCard
-                                        title={`${watchlist.auctionYear} ${watchlist.auctionMake} ${watchlist.auctionModel}`}
-                                        img={watchlist.auctionImage}
-                                        current_bid={watchlist.auctionPrice}
-                                        time_left={watchlist.auctionDeadline}
-                                        id={watchlist.auctionIdentifierId}
-                                    />
-                                </TimerProvider>
+                        {dataIsLoading
+                            ? <div className="tw-w-full tw-h-[100px] tw-flex tw-justify-center tw-items-center">
+                                <PulseLoader color='#f2ca16' />
                             </div>
-                        ))}
+                            : activeWatchlist.map((watchlist: any) => (
+                                <div key={watchlist._id}>
+                                    <TimerProvider deadline={watchlist.auctionDeadline}>
+                                        <MyWatchlistCard
+                                            title={`${watchlist.auctionYear} ${watchlist.auctionMake} ${watchlist.auctionModel}`}
+                                            img={watchlist.auctionImage}
+                                            current_bid={watchlist.auctionPrice}
+                                            time_left={watchlist.auctionDeadline}
+                                            id={watchlist.auctionIdentifierId}
+                                        />
+                                    </TimerProvider>
+                                </div>
+                            ))}
                     </div>
                 </div>
             </div>
@@ -273,87 +323,107 @@ function Profile(props: Props) {
 
 export default Profile;
 
-function UserWagerList() {
+
+type CompletedWagerCardProps = {
+    title: string;
+    img: string;
+    my_wager: number;
+    id: string;
+}
+
+const CompletedWagerCard: React.FC<CompletedWagerCardProps> = ({
+    title,
+    img,
+    my_wager,
+    id
+}) => {
     return (
-        <div className="tw-flex tw-gap-4 tw-py-3 tw-items-center">
-            <Image
-                src={WagerPhotoOne}
-                alt=""
-                className="tw-rounded tw-w-[100px]"
-            />
-            <div>
-                <div className="tw-font-bold sm:text-lg tw-leading-7">
-                    620-Mile 2019 Ford GT
-                </div>
-                <div className="tw-flex tw-gap-2 tw-text-sm tw-leading-5">
-                    <Image
-                        src={WalletIcon}
-                        alt=""
-                        className="tw-text-[#d1d3d6] tw-w-3.5"
-                    />
-                    <div className="tw-text-[#d1d3d6]">Wager:</div>
-                    <div className="tw-text-[#f2ca16] tw-font-bold">
-                        $999,000
+        <div>
+            <div className="tw-flex tw-gap-6 tw-py-6 tw-px-6 tw-items-center">
+                <Image
+                    src={img}
+                    width={100}
+                    height={100}
+                    alt={title}
+                    className="tw-rounded tw-w-[100px] tw-h-[100px] tw-object-cover"
+                />
+                <div className="tw-grid tw-gap-1.5">
+                    <div className="tw-font-bold tw-text-xl tw-leading-7">
+                        {title}
                     </div>
-                </div>
-                <div className="tw-flex tw-gap-2 tw-text-sm">
-                    <Image
-                        src={DollarIcon}
-                        alt=""
-                        className="tw-text-[#d1d3d6] tw-w-3.5"
-                    />
-                    <div className="tw-text-[#d1d3d6]">Bid:</div>
-                    <div className="tw-text-[#49c742] tw-font-bold">
-                        $904,000
+                    <div>
+                        <div className="tw-flex tw-gap-2 tw-leading-5">
+                            <Image
+                                src={WalletIcon}
+                                alt=""
+                                className="tw-text-[#d1d3d6] tw-w-3.5"
+                            />
+                            <div className="tw-text-[#d1d3d6]">Wager:</div>
+                            <div className="tw-text-[#f2ca16] tw-font-bold">
+                                ${my_wager}
+                            </div>
+                        </div>
+                        <div className="tw-flex tw-gap-2 tw-text-sm">
+                            <Image
+                                src={DollarIcon}
+                                alt=""
+                                className="tw-text-[#d1d3d6] tw-w-3.5"
+                            />
+                            <div className="tw-text-[#d1d3d6]">Winning Bid:</div>
+                            <div className="tw-text-[#49c742] tw-font-bold">
+                                --
+                            </div>
+                        </div>
+                        <div className="tw-flex tw-gap-2 tw-text-sm tw-items-center">
+                            <Image
+                                src={HourglassIcon}
+                                alt=""
+                                className="tw-text-[#d1d3d6] tw-w-3.5 tw-h-3.5"
+                            />
+                            <div className="tw-text-[#d1d3d6]">Status:</div>
+                            <div>Completed</div>
+                        </div>
                     </div>
-                </div>
-                <div className="tw-flex tw-gap-2 tw-text-sm tw-items-center">
-                    <Image
-                        src={HourglassIcon}
-                        alt=""
-                        className="tw-text-[#d1d3d6] tw-w-3.5 tw-h-3.5"
-                    />
-                    <div className="tw-text-[#d1d3d6]">Time Left:</div>
-                    <div>12:17:00</div>
                 </div>
             </div>
+            <div className="tw-h-[2px] tw-bg-white/10"></div>
         </div>
     );
 }
 
-function UserWatchList() {
-    return (
-        <div className="tw-flex tw-gap-4 tw-py-3 tw-items-center">
-            <Image
-                src={WagerPhotoOne}
-                alt=""
-                className="tw-rounded tw-w-[100px]"
-            />
-            <div>
-                <div className="tw-font-bold sm:text-lg tw-leading-7">
-                    620-Mile 2019 Ford GT
-                </div>
-                <div className="tw-flex tw-gap-2 tw-text-sm tw-leading-5">
-                    <Image
-                        src={DollarIcon}
-                        alt=""
-                        className="tw-text-[#d1d3d6] tw-w-3.5"
-                    />
-                    <div className="tw-text-[#d1d3d6]">Current Bid:</div>
-                    <div className="tw-text-[#49c742] tw-font-bold">
-                        $904,000
-                    </div>
-                </div>
-                <div className="tw-flex tw-gap-2 tw-text-sm tw-items-center">
-                    <Image
-                        src={HourglassIcon}
-                        alt=""
-                        className="tw-text-[#d1d3d6] tw-w-3.5 tw-h-3.5"
-                    />
-                    <div className="tw-text-[#d1d3d6]">Time Left:</div>
-                    <div>12:17:00</div>
-                </div>
-            </div>
-        </div>
-    );
-}
+// function UserWatchList() {
+//     return (
+//         <div className="tw-flex tw-gap-4 tw-py-3 tw-items-center">
+//             <Image
+//                 src={WagerPhotoOne}
+//                 alt=""
+//                 className="tw-rounded tw-w-[100px]"
+//             />
+//             <div>
+//                 <div className="tw-font-bold sm:text-lg tw-leading-7">
+//                     620-Mile 2019 Ford GT
+//                 </div>
+//                 <div className="tw-flex tw-gap-2 tw-text-sm tw-leading-5">
+//                     <Image
+//                         src={DollarIcon}
+//                         alt=""
+//                         className="tw-text-[#d1d3d6] tw-w-3.5"
+//                     />
+//                     <div className="tw-text-[#d1d3d6]">Current Bid:</div>
+//                     <div className="tw-text-[#49c742] tw-font-bold">
+//                         $904,000
+//                     </div>
+//                 </div>
+//                 <div className="tw-flex tw-gap-2 tw-text-sm tw-items-center">
+//                     <Image
+//                         src={HourglassIcon}
+//                         alt=""
+//                         className="tw-text-[#d1d3d6] tw-w-3.5 tw-h-3.5"
+//                     />
+//                     <div className="tw-text-[#d1d3d6]">Time Left:</div>
+//                     <div>12:17:00</div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
