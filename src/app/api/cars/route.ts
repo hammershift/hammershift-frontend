3ad945319f7c7a4c1016b5ea667c53d7a3703806
Amiel Brencis Salipande <absalipande@gmail.com>
2024-01-17 16:26:47 +0800

    Error validation message inside of login_page for invalid credentials

--- src/app/api/cars/route.ts
+++ src/app/api/cars/route.ts
@@ -1,9 +1,9 @@
-import clientPromise from "@/lib/mongodb";
-import connectToDB from "@/lib/mongoose";
-import Auctions from "@/models/auction.model";
-import { NextRequest, NextResponse } from "next/server";
+import clientPromise from '@/lib/mongodb';
+import connectToDB from '@/lib/mongoose';
+import Auctions from '@/models/auction.model';
+import { NextRequest, NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic';
 
 export async function GET(req: NextRequest) {
   try {
@@ -19,33 +19,54 @@ export async function GET(req: NextRequest) {
       return NextResponse.json(car);
     }
     // api/cars to get all cars
-    const cars = await db.collection('auctions').find({ $and: [{ isActive: true }] }).limit(limit).skip(offset);
+    const cars = await db
+      .collection('auctions')
+      .find({ $and: [{ isActive: true }] })
+      .limit(limit)
+      .skip(offset);
 
     const carsArray = await cars.toArray();
 
     return NextResponse.json({ total: carsArray.length, cars: carsArray });
   } catch (error) {
-    console.error(error)
-    return NextResponse.json({ message: "Internal server error" });
+    console.error(error);
+    return NextResponse.json({ message: 'Internal server error' });
   }
 }
 
+// export async function PUT(req: NextRequest) {
+//   try {
+//     await connectToDB();
+//     const auction_id = req.nextUrl.searchParams.get('auction_id');
+//     const edits = await req.json();
+
+//     const editedAuction = await Auctions.findOneAndUpdate({ $and: [{ auction_id: auction_id }, { isActive: true }] }
+//       ,
+//       { $set: edits },
+//       { new: true }
+//     )
+
+//     return NextResponse.json(editedAuction, { status: 202 });
+//   } catch (error) {
+//     console.error(error)
+//     return NextResponse.json({ message: "Internal server error" });
+//   }
+// }
 
 export async function PUT(req: NextRequest) {
   try {
-    await connectToDB();
+    const client = await clientPromise;
+    const db = client.db();
     const auction_id = req.nextUrl.searchParams.get('auction_id');
     const edits = await req.json();
 
-    const editedAuction = await Auctions.findOneAndUpdate({ $and: [{ auction_id: auction_id }, { isActive: true }] }
-      ,
-      { $set: edits },
-      { new: true }
-    )
+    const editedAuction = await db
+      .collection('auctions')
+      .findOneAndUpdate({ $and: [{ auction_id: auction_id }, { isActive: true }] }, { $set: edits }, { returnDocument: 'after' });
 
     return NextResponse.json(editedAuction, { status: 202 });
   } catch (error) {
-    console.error(error)
-    return NextResponse.json({ message: "Internal server error" });
+    console.error(error);
+    return NextResponse.json({ message: 'Internal server error' });
   }
 }
