cd5fce5124b4de74c639355335e5352b3f2fe833
Amiel Brencis Salipande <absalipande@gmail.com>
2024-01-30 19:14:32 +0800

    clean and formmatted the code inside of signup API

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -2,42 +2,6 @@ import clientPromise from '@/lib/mongodb';
 import bcrypt from 'bcrypt';
 import { NextResponse } from 'next/server';
 
-// export async function POST(req: Request) {
-//   const data = await req.json();
-//   const { email, password } = data;
-
-//   if (!email || !password || !email.includes('@') || password.trim().length < 7) {
-//     return NextResponse.json({ message: 'Invalid input' }, { status: 422 });
-//   }
-
-//   try {
-//     const client = await clientPromise;
-//     const db = client.db();
-
-//     const existingUser = await db.collection('users').findOne({ email });
-//     if (existingUser) {
-//       return NextResponse.json({ message: 'User already exists' }, { status: 422 });
-//     }
-
-//     const hashedPassword = await bcrypt.hash(password, 12);
-
-//     const newUserResult = await db.collection('users').insertOne({
-//       email,
-//       password: hashedPassword,
-//     });
-
-//     await db.collection('wallet').insertOne({
-//       userId: newUserResult.insertedId,
-//       balance: 100, // initial wallet balance
-//     });
-
-//     return NextResponse.json({ message: 'User and Wallet created' }, { status: 201 });
-//   } catch (error) {
-//     console.error('Error during registration process:', error);
-//     return NextResponse.json({ message: 'Invalid server error' }, { status: 500 });
-//   }
-// }
-
 export async function POST(req: Request) {
   const data = await req.json();
   const { email, password } = data;
@@ -63,8 +27,6 @@ export async function POST(req: Request) {
       password: hashedPassword,
       isActive: true,
       balance: 100, // initial wallet balance
-      // wagers: [], // test
-      // winnings: [], // test
     });
 
     return NextResponse.json({ message: 'User created with initial balance' }, { status: 201 });
