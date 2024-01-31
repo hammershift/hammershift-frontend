8fccf4cc20ce090e9890286b74f722e0fd7c6a1e
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2024-01-31 15:22:18 +0800

    fix: changed HTTP operation for auctionWinner

--- src/app/api/auctionWinner/route.ts
+++ src/app/api/auctionWinner/route.ts
@@ -13,7 +13,7 @@ import prizeDistribution from '@/helpers/prizeDistribution';
 import { refundWagers } from '@/helpers/refundWagers';
 import { updateWinnerWallet } from '@/helpers/updateWinnerWallet';
 
-export async function POST(req: NextRequest) {
+export async function GET(req: NextRequest) {
   // const session = await getServerSession(authOptions);
   // if (!session) {
   //   return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
@@ -118,7 +118,7 @@ export async function POST(req: NextRequest) {
   }
 }
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
   // const session = await getServerSession(authOptions);
   // if (!session) {
   //   return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
