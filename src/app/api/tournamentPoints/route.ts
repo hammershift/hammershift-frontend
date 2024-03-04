a322eacf76f4ea9c8acaab69ed1234c02b2754ab
Amiel Brencis Salipande <absalipande@gmail.com>
2024-03-04 09:16:57 +0800

    fix

--- src/app/api/tournamentPoints/route.ts
+++ src/app/api/tournamentPoints/route.ts
@@ -1,18 +1,58 @@
 import { calculateTournamentScores } from '@/helpers/calculateTournamentScores';
+import clientPromise from '@/lib/mongodb';
+import { ObjectId } from 'mongodb';
 import { NextRequest, NextResponse } from 'next/server';
 
+interface TournamentWager {
+  _id: ObjectId;
+  tournamentID: ObjectId;
+  wagers: Array<{
+    auctionID: ObjectId;
+    priceGuessed: number;
+  }>;
+  user: {
+    _id: ObjectId;
+    fullName: string;
+    username: string;
+    image: string;
+  };
+}
+
 export async function POST(req: NextRequest) {
-  try {
-    const { userWagers, auctions } = await req.json();
+  const client = await clientPromise;
+  const db = client.db();
 
-    if (!userWagers || !auctions) {
-      return NextResponse.json({ message: 'Invalid request' }, { status: 400 });
-    }
+  const tournamentId = req.nextUrl.searchParams.get('tournament_id');
 
-    const tournamentResults = calculateTournamentScores(userWagers, auctions);
-    return NextResponse.json({ tournamentResults });
-  } catch (error) {
-    console.error('Error in calculateTournamentScores:', error);
-    return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
+  if (!tournamentId) {
+    return NextResponse.json({ message: 'Tournament ID is required' }, { status: 400 });
   }
+
+  const tournamentWagersArray: TournamentWager[] = (await db
+    .collection('tournament_wagers')
+    .find({ tournamentID: new ObjectId(tournamentId) })
+    .toArray()) as TournamentWager[];
+
+  const auctionIDs = tournamentWagersArray.flatMap((wager) => wager.wagers.map((wager) => wager.auctionID));
+
+  const auctions = await db
+    .collection('auctions')
+    .find({ _id: { $in: auctionIDs } })
+    .toArray();
+
+  const auctionsFormatted = auctions.map((auction) => ({
+    _id: auction._id,
+    finalSellingPrice: auction.attributes.find((attr: { key: string }) => attr.key === 'price')?.value || 0,
+  }));
+
+  const userWagers = tournamentWagersArray.map((wager) => ({
+    userID: wager.user._id.toString(),
+    username: wager.user.username,
+    image: wager.user.image || '',
+    wagers: wager.wagers,
+  }));
+
+  const tournamentResults = calculateTournamentScores(userWagers, auctionsFormatted);
+
+  return NextResponse.json({ tournamentResults }, { status: 200 });
 }
