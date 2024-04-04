a17cb80f963c3b8ec6c65a3f50e3cf22ce74abcb
ae_jay <ajlimramirez@gmail.com>
2024-04-04 20:08:17 +0800

    small change

--- src/app/ui/tournaments/TournamentsList.tsx
+++ src/app/ui/tournaments/TournamentsList.tsx
@@ -2,12 +2,12 @@
 
 import React, { useEffect, useState } from "react";
 import {
-  getAllTournamentWagers,
-  getAuctionsByTournamentId,
-  getOneTournamentWager,
-  getSortedTournaments,
-  getTournamentPointsByTournamentId,
-  getTournaments,
+    getAllTournamentWagers,
+    getAuctionsByTournamentId,
+    getOneTournamentWager,
+    getSortedTournaments,
+    getTournamentPointsByTournamentId,
+    getTournaments,
 } from "@/lib/data";
 import { TimerProvider } from "@/app/_context/TimerContext";
 import Image from "next/image"; // Assuming you are using Next.js Image component
@@ -16,187 +16,198 @@ import dynamic from "next/dynamic";
 import { useSession } from "next-auth/react";
 
 const DynamicTournamentsCards = dynamic(
-  () => import("../../components/tournaments_card"),
-  {
-    ssr: false,
-    loading: () => (
-      <div className="tw-flex tw-mt-8 tw-justify-evenly tw-bg-gray-600 tw-rounded-lg tw-animate-pulse">
-        <div className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-m-2">
-          <div className="tw-w-96 tw-mb-2 tw-h-48 tw-bg-gray-700"></div>
-          <div className="tw-w-3/5 tw-h-6 tw-mb-2 tw-bg-gray-700 tw-rounded-lg tw-animate-pulse"></div>
-          <div className="tw-w-3/5 tw-mb-2 tw-h-6 tw-bg-gray-700 tw-rounded-lg tw-animate-pulse"></div>
-          <div className="tw-w-3/5 tw-mb-2 tw-h-6 tw-bg-gray-700 tw-rounded-lg tw-animate-pulse"></div>
-          <div className="tw-w-full tw-mb-2 tw-h-10 tw-bg-gray-700 tw-rounded-lg tw-animate-pulse"></div>
-        </div>
-      </div>
-    ),
-  }
+    () => import("../../components/tournaments_card"),
+    {
+        ssr: false,
+        loading: () => (
+            <div className="tw-flex tw-mt-8 tw-justify-evenly tw-bg-gray-600 tw-rounded-lg tw-animate-pulse">
+                <div className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-m-2">
+                    <div className="tw-w-96 tw-mb-2 tw-h-48 tw-bg-gray-700"></div>
+                    <div className="tw-w-3/5 tw-h-6 tw-mb-2 tw-bg-gray-700 tw-rounded-lg tw-animate-pulse"></div>
+                    <div className="tw-w-3/5 tw-mb-2 tw-h-6 tw-bg-gray-700 tw-rounded-lg tw-animate-pulse"></div>
+                    <div className="tw-w-3/5 tw-mb-2 tw-h-6 tw-bg-gray-700 tw-rounded-lg tw-animate-pulse"></div>
+                    <div className="tw-w-full tw-mb-2 tw-h-10 tw-bg-gray-700 tw-rounded-lg tw-animate-pulse"></div>
+                </div>
+            </div>
+        ),
+    }
 );
 
 export interface Tournaments {
-  status: number;
-  _id: string;
-  title: string;
-  pot: number;
-  endTime: Date;
-  tournamentEndTime: Date;
-  cars: number;
-  buyInFee: number;
-  // Add other properties of the tournament here
+    status: number;
+    _id: string;
+    title: string;
+    pot: number;
+    endTime: Date;
+    tournamentEndTime: Date;
+    cars: number;
+    buyInFee: number;
+    // Add other properties of the tournament here
 }
 
 interface Auctions {
-  _id: string;
-  image: string;
+    _id: string;
+    image: string;
 }
 
 interface AuctionScore {
-  auctionID: string;
-  score: number;
+    auctionID: string;
+    score: number;
 }
 
 interface TournamentPoints {
-  player: string;
-  points: number;
-  auctionScores: AuctionScore[];
+    player: string;
+    points: number;
+    auctionScores: AuctionScore[];
 }
 
 interface Wager {
-  // Define the properties of a wager here
-  // For example:
-  id: string;
-  amount: number;
+    // Define the properties of a wager here
+    // For example:
+    id: string;
+    amount: number;
 }
 
 const TournamentsList = () => {
-  const [tournamentsData, setTournamentsData] = useState<Tournaments[]>([]);
-  const [totalTournaments, setTotalTournaments] = useState(0);
-  const [auctionData, setAuctionData] = useState<Record<string, Auctions[]>>(
-    {}
-  );
-  const [sortType, setSortType] = useState<string>("newest");
-  const [tournamentLimit, setTournamentLimit] = useState(6);
-  const [playerLimit, setPlayerLimit] = useState(3);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [tournamentPointsData, setTournamentPointsData] = useState<
-    TournamentPoints[]
-  >([]);
+    const [tournamentsData, setTournamentsData] = useState<Tournaments[]>([]);
+    const [totalTournaments, setTotalTournaments] = useState(0);
+    const [auctionData, setAuctionData] = useState<Record<string, Auctions[]>>(
+        {}
+    );
+    const [sortType, setSortType] = useState<string>("newest");
+    const [tournamentLimit, setTournamentLimit] = useState(6);
+    const [playerLimit, setPlayerLimit] = useState(3);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [tournamentPointsData, setTournamentPointsData] = useState<
+        TournamentPoints[]
+    >([]);
 
-  useEffect(() => {
-    const fetchTournamentsData = async () => {
-      setLoading(true);
-      try {
-        const data = await getSortedTournaments(sortType, tournamentLimit);
-        const tournamentsArray = data.tournaments;
-        if (data) {
-          setTournamentsData(tournamentsArray);
-          setTotalTournaments(data?.total);
+    useEffect(() => {
+        const fetchTournamentsData = async () => {
+            setLoading(true);
+            try {
+                const data = await getSortedTournaments(
+                    sortType,
+                    tournamentLimit
+                );
+                const tournamentsArray = data.tournaments;
+                if (data) {
+                    setTournamentsData(tournamentsArray);
+                    setTotalTournaments(data?.total);
 
-          // Fetch tournament points data for each tournament
-          const tournamentPointsPromises = tournamentsArray.map(
-            (tournament: { _id: string }) =>
-              getTournamentPointsByTournamentId(tournament._id, playerLimit)
-          );
-          const tournamentPointsArray = await Promise.all(
-            tournamentPointsPromises
-          );
-          setTournamentPointsData(tournamentPointsArray);
+                    // Fetch tournament points data for each tournament
+                    const tournamentPointsPromises = tournamentsArray.map(
+                        (tournament: { _id: string }) =>
+                            getTournamentPointsByTournamentId(
+                                tournament._id,
+                                playerLimit
+                            )
+                    );
+                    const tournamentPointsArray = await Promise.all(
+                        tournamentPointsPromises
+                    );
+                    setTournamentPointsData(tournamentPointsArray);
 
-          setLoading(false);
-          return;
-        }
-        setLoading(false);
-      } catch (error) {
-        console.error("Failed to fetch tournament data:", error);
-      }
+                    setLoading(false);
+                    return;
+                }
+                setLoading(false);
+            } catch (error) {
+                console.error("Failed to fetch tournament data:", error);
+            }
+        };
+        fetchTournamentsData();
+    }, [sortType, tournamentLimit, playerLimit]);
+    console.log("tournaments data: ", tournamentsData);
+
+    useEffect(() => {
+        const fetchAuctionData = async () => {
+            try {
+                const auctionsByTournament: Record<string, Auctions[]> = {};
+                for (const tournament of tournamentsData) {
+                    const auctionDataForTournament =
+                        await getAuctionsByTournamentId(tournament._id);
+                    auctionsByTournament[tournament._id] =
+                        auctionDataForTournament;
+                }
+                setAuctionData(auctionsByTournament);
+            } catch (error) {
+                console.error("Failed to fetch auction data:", error);
+            }
+        };
+        fetchAuctionData();
+    }, [tournamentsData]);
+
+    const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSortType(event.target.value);
     };
-    fetchTournamentsData();
-  }, [sortType, tournamentLimit, playerLimit]);
-  console.log("tournaments data: ", tournamentsData);
 
-  useEffect(() => {
-    const fetchAuctionData = async () => {
-      try {
-        const auctionsByTournament: Record<string, Auctions[]> = {};
-        for (const tournament of tournamentsData) {
-          const auctionDataForTournament = await getAuctionsByTournamentId(
-            tournament._id
-          );
-          auctionsByTournament[tournament._id] = auctionDataForTournament;
-        }
-        setAuctionData(auctionsByTournament);
-      } catch (error) {
-        console.error("Failed to fetch auction data:", error);
-      }
+    const handleLoadMore = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setTournamentLimit((prev) => prev + 3);
     };
-    fetchAuctionData();
-  }, [tournamentsData]);
 
-  const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSortType(event.target.value);
-  };
-
-  const handleLoadMore = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setTournamentLimit((prev) => prev + 3);
-  };
-
-  return (
-    <div>
-      <div className="tw-mt-5">
-        <div className="tw-flex tw-justify-between tw-items-center">
-          {" "}
-          <div className="tw-font-bold tw-text-3xl md:tw-text-5xl">
-            Tournaments
-          </div>
-          <select
-            className="tw-inline-flex tw-justify-between tw-items-center tw-gap-x-1.5 tw-rounded-md tw-px-3 tw-py-2.5  tw-text-white-900 tw-shadow-sm tw-bg-[#172431] hover:tw-bg-[#1A2C3D] tw-truncate"
-            onChange={handleSortChange}
-          >
-            <option value="newest">Newly Listed</option>
-            <option value="endingSoon">Ending Soon</option>
-          </select>
+    return (
+        <div>
+            <div className="tw-mt-5">
+                <div className="tw-flex tw-justify-between tw-items-center">
+                    {" "}
+                    <div className="tw-font-bold tw-text-3xl md:tw-text-5xl">
+                        Tournaments
+                    </div>
+                    <select
+                        className="tw-inline-flex tw-justify-between tw-items-center tw-gap-x-1.5 tw-rounded-md tw-px-3 tw-py-2.5  tw-text-white-900 tw-shadow-sm tw-bg-[#172431] hover:tw-bg-[#1A2C3D] tw-truncate"
+                        onChange={handleSortChange}
+                    >
+                        <option value="newest">Newly Listed</option>
+                        <option value="endingSoon">Ending Soon</option>
+                    </select>
+                </div>
+            </div>
+            <div className="tw-grid tw-grid-cols-2 max-sm:tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-x-4 md:tw-gap-x-[64px] tw-gap-y-8 md:tw-gap-y-16 tw-pb-20 tw-mt-12">
+                {tournamentsData &&
+                    tournamentsData.map((tournament, index) => {
+                        const imagesForTournament =
+                            auctionData[tournament._id]?.map(
+                                (auction) => auction.image
+                            ) || [];
+                        const tournamentPoints = tournamentPointsData[index];
+                        return (
+                            <div key={tournament._id}>
+                                <TimerProvider deadline={tournament.endTime}>
+                                    <DynamicTournamentsCards
+                                        tournament_id={tournament._id}
+                                        pot={tournament.pot}
+                                        title={tournament.title}
+                                        deadline={tournament.endTime}
+                                        tournament_deadline={
+                                            tournament.tournamentEndTime
+                                        }
+                                        images={imagesForTournament}
+                                        tournamentPoints={tournamentPoints}
+                                        canceledTournament={
+                                            tournament.status === 3
+                                        }
+                                    />
+                                </TimerProvider>
+                            </div>
+                        );
+                    })}
+            </div>
+            <div>
+                <div className="tw-text-[18px] tw-opacity-50 tw-text-center tw-mb-4">
+                    {tournamentLimit < totalTournaments
+                        ? `Showing ${tournamentLimit} of ${totalTournaments} tournaments`
+                        : `Showing ${totalTournaments} of ${totalTournaments} tournaments`}
+                </div>
+                <button
+                    className={`btn-transparent-white tw-w-full tw-text-[18px] tw-mb-8`}
+                    onClick={handleLoadMore}
+                >
+                    Load More
+                </button>
+            </div>
         </div>
-      </div>
-      <div className="tw-grid tw-grid-cols-2 max-sm:tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-x-4 md:tw-gap-x-[64px] tw-gap-y-8 md:tw-gap-y-16 tw-mt-12 tw-pb-20">
-        {tournamentsData &&
-          tournamentsData.map((tournament, index) => {
-            const imagesForTournament =
-              auctionData[tournament._id]?.map((auction) => auction.image) ||
-              [];
-            const tournamentPoints = tournamentPointsData[index];
-            return (
-              <div key={tournament._id}>
-                <TimerProvider deadline={tournament.endTime}>
-                  <DynamicTournamentsCards
-                    tournament_id={tournament._id}
-                    pot={tournament.pot}
-                    title={tournament.title}
-                    deadline={tournament.endTime}
-                    tournament_deadline={tournament.tournamentEndTime}
-                    images={imagesForTournament}
-                    tournamentPoints={tournamentPoints}
-                    canceledTournament={tournament.status === 3}
-                  />
-                </TimerProvider>
-              </div>
-            );
-          })}
-      </div>
-      <div>
-        <div className="tw-text-[18px] tw-opacity-50 tw-text-center tw-mb-4">
-          {tournamentLimit < totalTournaments
-            ? `Showing ${tournamentLimit} of ${totalTournaments} tournaments`
-            : `Showing ${totalTournaments} of ${totalTournaments} tournaments`}
-        </div>
-        <button
-          className={`btn-transparent-white tw-w-full tw-text-[18px] tw-mb-8`}
-          onClick={handleLoadMore}
-        >
-          Load More
-        </button>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default TournamentsList;
