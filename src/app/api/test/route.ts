7cadd31b185be3503fbdc0a2ba906a1a1f667eb8
Amiel Brencis Salipande <absalipande@gmail.com>
2024-04-16 16:55:25 +0800

    fix: Removed unnecessary import inside of test API

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,25 +1,11 @@
-import prizeAlgorithm from '@/helpers/prizeAlgorithm';
 import prizeDistribution from '@/helpers/prizeDistribution';
 import { NextRequest, NextResponse } from 'next/server';
-
-// export async function POST(req: NextRequest) {
-//   try {
-//     const { wagers, finalSellingPrice, totalPot } = await req.json();
-
-//     const winners = prizeDistribution(wagers, finalSellingPrice, totalPot);
-
-//     return NextResponse.json({ winners });
-//   } catch (error) {
-//     console.error('Error in testPrizeDistribution:', error);
-//     return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
-//   }
-// }
 
 export async function POST(req: NextRequest) {
   try {
     const { wagers, finalSellingPrice, totalPot } = await req.json();
 
-    const winners = prizeAlgorithm(wagers, finalSellingPrice, totalPot);
+    const winners = prizeDistribution(wagers, finalSellingPrice, totalPot);
 
     return NextResponse.json({ winners });
   } catch (error) {
