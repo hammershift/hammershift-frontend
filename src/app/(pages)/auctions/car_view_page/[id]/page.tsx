bd8e7bd43b8bb352a60a791a8e79d2252f5bf165
ae_jay <ajlimramirez@gmail.com>
2024-04-23 10:24:18 +0800

    removed inactive auction wagers in the wagers section

--- src/app/(pages)/auctions/car_view_page/[id]/page.tsx
+++ src/app/(pages)/auctions/car_view_page/[id]/page.tsx
@@ -119,9 +119,15 @@ const CarViewPage = ({ params }: { params: { id: string } }) => {
                     data?._id,
                     session?.user.id
                 );
+                console.log(userWager);
+
                 !userWager.length
                     ? setAlreadyWagered(false)
                     : setAlreadyWagered(true);
+
+                if (userWager.length) {
+                    !userWager[0].isActive && setAlreadyWagered(false);
+                }
             }
 
             if (data) {
