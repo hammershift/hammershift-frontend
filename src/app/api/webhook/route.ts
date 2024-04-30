e7501b3766d23388651322b15d701982739524fa
Amiel Brencis Salipande <absalipande@gmail.com>
2024-04-30 18:22:19 +0800

    small changes

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,56 +3,50 @@ import { NextRequest, NextResponse } from 'next/server';
 import clientPromise from '@/lib/mongodb';
 import Transaction from '@/models/transaction';
 import mongoose from 'mongoose';
+import { headers } from 'next/headers';
+import { stripe } from '@/lib/stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-
-export async function POST(req: NextRequest, res: NextResponse) {
-  const payload = await req.text();
-  const signature = req.headers.get('Stripe-Signature');
-
+export async function POST(req: NextRequest) {
   try {
-    let event = stripe.webhooks.constructEvent(payload, signature!, process.env.STRIPE_WEBHOOK_SECRET!);
+    const signature = headers().get('Stripe-Signature') as string;
+    const payload = await req.text();
 
-    console.log('event', event.type);
+    let event: Stripe.Event;
 
-    // Event handlers
-    switch (event.type) {
-      case 'payment_intent.created':
-        console.log('PaymentIntent was created.');
-        break;
-      case 'payment_intent.succeeded':
-        const client = await clientPromise;
-        const db = client.db();
+    event = stripe.webhooks.constructEvent(payload, signature, process.env.STRIPE_WEBHOOK_SECRET!);
 
-        const stripeCustomerId = event.data.object.customer;
-        const amountPaid = event.data.object.amount_received / 100;
-        const userId = event.data.object.metadata.userId;
+    if (event.type === 'payment_intent.succeeded') {
+      const client = await clientPromise;
+      const db = client.db();
 
-        const updateUserBalance = await db.collection('users').updateOne({ stripeCustomerId: stripeCustomerId }, { $inc: { balance: amountPaid } });
+      const amountPaid = event.data.object.amount_received / 100;
+      const userId = event.data.object.metadata.userId;
 
-        console.log(`Updated user balance for: ${stripeCustomerId}:`, updateUserBalance);
+      if (!userId || amountPaid === 0) {
+        throw new Error('Invalid userId or amountPaid');
+      }
 
-        const depositTransaction = new Transaction({
-          userID: new mongoose.Types.ObjectId(userId),
-          transactionType: 'deposit',
-          amount: amountPaid,
-          type: '+',
-          transactionDate: new Date(),
-        });
+      const updateResult = await db.collection('users').updateOne({ _id: new mongoose.Types.ObjectId(userId) }, { $inc: { balance: amountPaid } });
+      if (updateResult.modifiedCount === 0) {
+        throw new Error('User balance update failed');
+      }
 
-        const createDepositTransaction = await db.collection('transactions').insertOne(depositTransaction);
-        console.log(`Created deposit transaction for user id: ${userId} with stripe ID: ${stripeCustomerId}:`, createDepositTransaction);
+      const depositTransaction = new Transaction({
+        userID: new mongoose.Types.ObjectId(userId),
+        transactionType: 'deposit',
+        amount: amountPaid,
+        type: '+',
+        transactionDate: new Date(),
+      });
 
-        break;
-      case 'payment_intent.payment_failed':
-        console.log('PaymentIntent failed.');
-        break;
-      default:
-        console.log(`Unhandled event type ${event.type}`);
+      const createDepositTransaction = await db.collection('transactions').insertOne(depositTransaction);
+      console.log(`Created deposit transaction for user id: ${userId}:`, createDepositTransaction);
+
+      return NextResponse.json({ message: 'Success' }, { status: 200 });
     }
-    return NextResponse.json({ status: 'Success', event: event.type });
-  } catch (error) {
-    console.error('Error handling Stripe webhook event:', error);
-    return NextResponse.json({ status: 'Failed', error: error });
+    return NextResponse.json({ message: 'Unhandled event type' }, { status: 200 });
+  } catch (err: any) {
+    console.error(err);
+    return NextResponse.json({ message: err.message }, { status: 500 });
   }
 }
