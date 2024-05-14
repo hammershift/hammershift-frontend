e8e6ced25f994bf8ce5e9b8aba28d62ba6fe0c5f
ae_jay <ajlimramirez@gmail.com>
2024-05-14 09:35:37 +0800

    image fix

--- next.config.js
+++ next.config.js
@@ -9,13 +9,31 @@ const nextConfig = {
                 source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Origin", value: "*" },
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
             }
         ]
-    }
-}
+    },
+    webpack: (config, { isServer }) => {
+        config.module.rules.push({
+            test: /\.(png|jpe?g|gif|svg)$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        publicPath: '/_next/static/images',
+                        outputPath: 'static/images',
+                        name: '[name].[hash].[ext]',
+                        esModule: false,
+                    },
+                },
+            ],
+        });
+
+        return config;
+    },
+};
 
 module.exports = nextConfig;
