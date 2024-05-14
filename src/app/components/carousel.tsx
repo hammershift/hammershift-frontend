8ac8b578d323995e3640f314e60134a068c8bb5f
ae_jay <ajlimramirez@gmail.com>
2024-05-14 10:30:53 +0800

    banner ad image fix

--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -8,9 +8,6 @@ import ArrowRight from "../../../public/images/arrow-right.svg";
 import ArrowLeft from "../../../public/images/arrow-left.svg";
 import DiagonalLinesCarousel from "../../../public/images/diagonal-lines-carousel.svg";
 import Link from "next/link";
-import BannerAdOne from "@/../public/images/Banner_Ad.jpg";
-import BannerAdTwo from "@/../public/images/Banner_Ad2.jpg";
-import BannerAdThree from "@/../public/images/Banner_Ad3.jpg";
 
 const Carousel = () => {
     const [sliderTransform, setSlidertransform] = useState(0);
@@ -47,34 +44,37 @@ const Carousel = () => {
                 >
                     <SlideOne />
                     <div className="tw-basis-full tw-flex tw-justify-center tw-items-center">
-                        <Image
+                        {/* <Image
                             src="/images/Banner_Ad.jpg"
                             alt="banner ad"
                             width={0}
                             height={0}
                             sizes="100vw"
                             style={{ width: "100%", height: "auto" }}
-                        />
+                        /> */}
+                        <img src="/images/Banner_Ad.jpg" alt="car" />
                     </div>
                     <div className="tw-basis-full tw-flex tw-justify-center tw-items-center">
-                        <Image
+                        {/* <Image
                             src="/images/Banner_Ad2.jpg"
                             alt="banner ad"
                             width={0}
                             height={0}
                             sizes="100vw"
                             style={{ width: "100%", height: "auto" }}
-                        />
+                        /> */}
+                        <img src="/images/Banner_Ad2.jpg" alt="car" />
                     </div>
                     <div className="tw-basis-full tw-flex tw-justify-center tw-items-center">
-                        <Image
+                        {/* <Image
                             src="/images/Banner_Ad3.jpg"
                             alt="banner ad"
                             width={0}
                             height={0}
                             sizes="100vw"
                             style={{ width: "100%", height: "auto" }}
-                        />
+                        /> */}
+                        <img src="/images/Banner_Ad3.jpg" alt="car" />
                     </div>
                     <div className="tw-basis-full tw-flex tw-justify-center tw-items-center">
                         Section 5
