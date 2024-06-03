5b38abbac647e9bc435979cb8491f6293d847316
rrmgarcia <ronaldravengarcia@gmail.com>
2024-06-03 20:51:58 +0800

    modified code for footer to include links of sitemap, added new pages: terms of service and privacy policy, modified code for my_wallet page and added condition to disable button temporarily

---
+++ src/app/(pages)/privacy_policy/page.tsx
@@ -0,0 +1,142 @@
+import Footer from "@/app/components/footer";
+import React from "react";
+
+const PrivacyPolicyPage = () => {
+  return (
+    <div>
+      <div className="page-container tw-gap-5">
+        <div className="tw-mb-10">
+          <h1 className="tw-font-bold tw-text-3xl md:tw-text-5xl">
+            Privacy Policy
+          </h1>
+        </div>
+        <div className="section-container">
+          <h3 className="tw-text-xl tw-py-2 tw-font-bold">Introduction</h3>
+          <p className="tw-px-2">
+            This Privacy Policy explains how HammerShift collects, uses, and
+            protects your personal information. By using our Service, you agree
+            to the collection and use of information in accordance with this
+            policy.
+          </p>
+        </div>
+        <div className="section-container">
+          <h3 className="tw-text-xl tw-py-2 tw-font-bold">
+            Information Collection
+          </h3>
+          <p className="tw-px-2">
+            We collect the following types of information:
+          </p>
+          <ul className="tw-px-2">
+            <li>
+              Personal Information: Name, email address, and payment information
+              when you create an account and participate in wagering activities.
+            </li>
+            <li>
+              Usage Data: Information on how you access and use the Service,
+              including your IP address, browser type, and device information.
+            </li>
+          </ul>
+        </div>
+        <div className="section-container">
+          <h3 className="tw-text-xl tw-py-2 tw-font-bold">
+            Use of Information
+          </h3>
+          <p className="tw-px-2">
+            We use the collected information for the following purposes:
+          </p>
+          <ul className="tw-px-2">
+            <li>To provide and maintain the Service.</li>
+            <li>To process wagers and manage your account.</li>
+            <li>
+              To communicate with you, including sending updates and promotional
+              materials.
+            </li>
+            <li>To improve our services and develop new features.</li>
+          </ul>
+        </div>
+        <div className="section-container">
+          <h3 className="tw-text-xl tw-py-2 tw-font-bold"> Data Sharing</h3>
+          <p className="tw-px-2">
+            We do not share your personal information with third parties except
+            as necessary to process payments or comply with legal obligations.
+            We may share aggregated, non-personally identifiable information
+            with third parties for research and analysis purposes.
+          </p>
+        </div>
+        <div className="section-container">
+          <h3 className="tw-text-xl tw-py-2 tw-font-bold">Data Security</h3>
+          <p className="tw-px-2">
+            We implement industry-standard security measures to protect your
+            personal information from unauthorized access, disclosure,
+            alteration, or destruction. However, no method of transmission over
+            the internet or electronic storage is 100% secure.
+          </p>
+        </div>
+        <div className="section-container">
+          <h3 className="tw-text-xl tw-py-2 tw-font-bold">User Rights</h3>
+          <p className="tw-px-2">
+            You have the right to access, correct, or delete your personal
+            information. To exercise these rights, please contact us at [email].
+            We will respond to your request within a reasonable timeframe.
+          </p>
+        </div>
+        <div className="section-container">
+          <h3 className="tw-text-xl tw-py-2 tw-font-bold">
+            Cookies and Tracking
+          </h3>
+          <p className="tw-px-2">
+            We use cookies and similar tracking technologies to enhance your
+            experience and gather usage data. You can control cookie settings
+            through your browser. Disabling cookies may affect the functionality
+            of the Service.
+          </p>
+        </div>
+        <div className="section-container">
+          <h3 className="tw-text-xl tw-py-2 tw-font-bold">
+            Children&apos;s Privacy
+          </h3>
+          <p className="tw-px-2">
+            Our services are not intended for children under 18, and we do not
+            knowingly collect data from minors. If we become aware that we have
+            collected personal information from a child under 18, we will take
+            steps to delete such information.
+          </p>
+        </div>
+        <div className="section-container">
+          <h3 className="tw-text-xl tw-py-2 tw-font-bold">
+            Limitation of Liability
+          </h3>
+          <p className="tw-px-2">
+            HammerShift is not liable for any indirect, incidental, or
+            consequential damages arising from the use of our Service. The total
+            liability of HammerShift for any claims under these terms is limited
+            to the amount paid by the user to use the Service.
+          </p>
+        </div>
+        <div className="section-container">
+          <h3 className="tw-text-xl tw-py-2 tw-font-bold">
+            Changes to the Privacy Policy
+          </h3>
+          <p className="tw-px-2">
+            We may update this Privacy Policy from time to time. Changes will be
+            posted on our website, and significant changes will be communicated
+            via email. Your continued use of the Service after any changes
+            indicates your acceptance of the new policy.
+          </p>
+        </div>
+        <div className="section-container">
+          <h3 className="tw-text-xl tw-py-2 tw-font-bold">
+            Contact Information
+          </h3>
+          <p className="tw-px-2">
+            If you have any questions about these Terms of Service, please
+            contact us at [email].
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default PrivacyPolicyPage;
--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,119 +5,118 @@ import FacebookIcon from "../../../public/images/social-icons/facebook-icon.svg"
 import LinkedinIcon from "../../../public/images/social-icons/linkedin-icon.svg";
 import PinterestIcon from "../../../public/images/social-icons/pinterest-icon.svg";
 import TwitterIcon from "../../../public/images/social-icons/twitter-icon.svg";
+import Link from "next/link";
 
 const companyList = [
-    "HOW IT WORKS",
-    "ABOUT",
-    "PRESS KIT",
-    "PRIVACY NOTICE",
-    "TERMS & CONDITIONS",
-    "CONTRACT",
+  { title: "HOW IT WORKS", link: "/how_it_works" },
+  { title: "ABOUT", link: "/about_page" },
+  { title: "PRESS KIT", link: "/" },
+  { title: "PRIVACY NOTICE", link: "/privacy_policy" },
+  { title: "TERMS & CONDITIONS", link: "/tos" },
+  { title: "CONTRACT", link: "/" },
 ];
 const productList = [
-    "DISCOVER",
-    "AUCTIONS",
-    "NEWLY LISTED",
-    "MOST EXPENSIVE",
-    "MOST BIDS",
-    "ENDING SOON",
-    "MOST WAGERS",
+  { title: "DISCOVER", link: "/discover" },
+  { title: "AUCTIONS", link: "/auctions" },
+  { title: "NEWLY LISTED", link: "/explore" },
+  { title: "MOST EXPENSIVE", link: "/explore" },
+  { title: "MOST BIDS", link: "/explore" },
+  { title: "ENDING SOON", link: "/explore" },
+  { title: "MOST WAGERS", link: "/explore" },
 ];
 const vehiclesList = ["MAKE", "TYPE", "ERA", "LOCATION"];
 
 const Footer = () => {
-    return (
-        <div className="section-container tw-pt-16 md:tw-pt-[120px] tw-pb-16 md:tw-pb-20">
-            <div className="tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-8 md:tw-gap-0 tw-mb-16">
-                <div>
-                    <Image
-                        src={Logo}
-                        width={177}
-                        height={32}
-                        alt="hammershift logo"
-                        className="tw-w-[177px] tw-h-[32px]"
-                    />
-                </div>
-                <div className="tw-grid tw-grid-cols-1 md:tw-grid-cols-3 tw-gap-8 md:tw-gap-0">
-                    <div>
-                        <div className="tw-font-bold">COMPANY</div>
-                        {companyList.map((item) => {
-                            return (
-                                <div key={item} className="tw-mt-3">
-                                    {item}
-                                </div>
-                            );
-                        })}
-                    </div>
-                    <div>
-                        <div className="tw-font-bold">PRODUCT</div>
-                        {productList.map((item) => {
-                            return (
-                                <div key={item} className="tw-mt-3">
-                                    {item}
-                                </div>
-                            );
-                        })}
-                    </div>
-                    <div>
-                        <div className="tw-font-bold">VEHICLES</div>
-                        {vehiclesList.map((item) => {
-                            return (
-                                <div key={item} className="tw-mt-3">
-                                    {item}
-                                </div>
-                            );
-                        })}
-                    </div>
-                </div>
-            </div>
-            <hr style={{ borderColor: "white", opacity: "10%" }} />
-            <div className="tw-flex tw-flex-col md:tw-flex-row tw-justify-between tw-items-end">
-                <div className="tw-my-8">
-                    <div className="tw-font-bold tw-text-xl">
-                        Shift Your Perspective, Hammer Down on Your Predictions
-                    </div>
-                    <div>Skill-based wagering for those in the know</div>
-                    <div className="tw-pt-6 tw-flex">
-                        <Image
-                            src={TwitterIcon}
-                            width={24}
-                            height={24}
-                            alt="twitter logo"
-                            className="tw-w-6 tw-h-6 tw-mr-6"
-                        />
-                        <Image
-                            src={LinkedinIcon}
-                            width={24}
-                            height={24}
-                            alt="twitter logo"
-                            className="tw-w-6 tw-h-6 tw-mr-6"
-                        />
-                        <Image
-                            src={FacebookIcon}
-                            width={24}
-                            height={24}
-                            alt="twitter logo"
-                            className="tw-w-6 tw-h-6 tw-mr-6"
-                        />
-                        <Image
-                            src={PinterestIcon}
-                            width={24}
-                            height={24}
-                            alt="twitter logo"
-                            className="tw-w-6 tw-h-6 tw-mr-6"
-                        />
-                    </div>
-                </div>
-                <div className="tw-flex tw-flex-col tw-items-end">
-                    <div className="tw-opacity-20">
-                        Designed by Toffeenut Design
-                    </div>
-                    <div>© 2023 HammerShift. All rights reserved.</div>
-                </div>
-            </div>
+  return (
+    <div className="section-container tw-pt-16 md:tw-pt-[120px] tw-pb-16 md:tw-pb-20">
+      <div className="tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-8 md:tw-gap-0 tw-mb-16">
+        <div>
+          <Image
+            src={Logo}
+            width={177}
+            height={32}
+            alt="hammershift logo"
+            className="tw-w-[177px] tw-h-[32px]"
+          />
         </div>
-    );
+        <div className="tw-grid tw-grid-cols-1 md:tw-grid-cols-3 tw-gap-8 md:tw-gap-0">
+          <div>
+            <div className="tw-font-bold">COMPANY</div>
+            {companyList.map((item) => {
+              return (
+                <div key={item.title} className="tw-mt-3">
+                  <Link href={item.link}>{item.title}</Link>
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            <div className="tw-font-bold">PRODUCT</div>
+            {productList.map((item) => {
+              return (
+                <div key={item.title} className="tw-mt-3">
+                  <Link href={item.link}>{item.title}</Link>
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            <div className="tw-font-bold">VEHICLES</div>
+            {vehiclesList.map((item) => {
+              return (
+                <div key={item} className="tw-mt-3">
+                  {item}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <hr style={{ borderColor: "white", opacity: "10%" }} />
+      <div className="tw-flex tw-flex-col md:tw-flex-row tw-justify-between tw-items-end">
+        <div className="tw-my-8">
+          <div className="tw-font-bold tw-text-xl">
+            Shift Your Perspective, Hammer Down on Your Predictions
+          </div>
+          <div>Skill-based wagering for those in the know</div>
+          <div className="tw-pt-6 tw-flex">
+            <Image
+              src={TwitterIcon}
+              width={24}
+              height={24}
+              alt="twitter logo"
+              className="tw-w-6 tw-h-6 tw-mr-6"
+            />
+            <Image
+              src={LinkedinIcon}
+              width={24}
+              height={24}
+              alt="twitter logo"
+              className="tw-w-6 tw-h-6 tw-mr-6"
+            />
+            <Image
+              src={FacebookIcon}
+              width={24}
+              height={24}
+              alt="twitter logo"
+              className="tw-w-6 tw-h-6 tw-mr-6"
+            />
+            <Image
+              src={PinterestIcon}
+              width={24}
+              height={24}
+              alt="twitter logo"
+              className="tw-w-6 tw-h-6 tw-mr-6"
+            />
+          </div>
+        </div>
+        <div className="tw-flex tw-flex-col tw-items-end">
+          <div className="tw-opacity-20">Designed by Toffeenut Design</div>
+          <div>© 2023 HammerShift. All rights reserved.</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
