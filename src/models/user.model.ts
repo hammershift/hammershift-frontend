e018fd6b6f058af555a8fe7fe5346aee3511ce9c
Ramon Arca <ramonteodoroarca@gmail.com>
2025-04-10 20:19:28 +0800

    Fix conflicts

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,31 +1,31 @@
 import { Document, Schema, model, models } from "mongoose";
 
 export interface User {
-    username: string;
-    fullName: string;
-    email: string;
-    balance: number;
-    isActive: boolean;
-    isBanned: boolean;
-    provider: string;
-    createdAt: Date,
-    updatedAt: Date,
+  username: string;
+  fullName: string;
+  email: string;
+  balance: number;
+  isActive: boolean;
+  isBanned: boolean;
+  provider: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 const userSchema = new Schema(
-    {
-        _id: { type: Schema.Types.ObjectId, required: true },
-        username: { type: String, required: true },
-        fullName: { type: String, required: true },
-        email: { type: String, required: true },
-        balance: { type: Number, default: 0 },
-        isActive: { type: Boolean, default: true },
-        isBanned: { type: Boolean, default: false },
-        provider: { type: String, default: 'credentials' },
-        createdAt: Date,
-        updatedAt: Date,
-    },
-    { collection: "users", timestamps: true }
+  {
+    _id: { type: Schema.Types.ObjectId, required: true },
+    username: { type: String, required: true },
+    fullName: { type: String, required: true },
+    email: { type: String, required: true },
+    balance: { type: Number, default: 0 },
+    isActive: { type: Boolean, default: true },
+    isBanned: { type: Boolean, default: false },
+    provider: { type: String, default: "credentials" },
+    createdAt: Date,
+    updatedAt: Date,
+  },
+  { collection: "users", timestamps: true }
 );
 
 const Users = models.users || model<User>("users", userSchema);
