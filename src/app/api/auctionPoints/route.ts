bd5e7803ade64640b7be339b94aa365183cc3f86
Ramon Arca <ramonteodoroarca@gmail.com>
2025-04-22 22:37:23 +0800

    Add totalPredictions to auction points route
    Change to use mongoose as well

--- src/app/api/auctionPoints/route.ts
+++ src/app/api/auctionPoints/route.ts
@@ -1,38 +1,47 @@
-import clientPromise from '@/lib/mongodb';
-import mongoose from 'mongoose';
-import { NextRequest, NextResponse } from 'next/server';
-
+import clientPromise from "@/lib/mongodb";
+import connectToDB from "@/lib/mongoose";
+import mongoose from "mongoose";
+import { NextRequest, NextResponse } from "next/server";
+import AuctionPoints from "@/models/auction_points";
 export async function GET(req: NextRequest) {
   try {
-    const client = await clientPromise;
-    const db = client.db();
+    // const client = await clientPromise;
+    // const db = client.db();
+    await connectToDB();
     const limit = Number(req.nextUrl.searchParams.get("limit"));
 
-    const leaderboardData = await db
-      .collection('auction_points')
-      .aggregate([
-        {
-          $group: {
-            _id: '$userID',
-            totalPoints: { $sum: '$points' },
-            fullName: { $first: '$fullName' },
-            username: { $first: '$username' },
-            image: { $first: '$image' },
-            auctions: { $push: { auctionID: '$auctionID', points: '$points', rank: '$rank' } },
+    const leaderboardData = await AuctionPoints.aggregate([
+      {
+        $group: {
+          _id: "$userId",
+
+          totalPoints: { $sum: "$points" },
+          totalPredictions: { $sum: 1 },
+          fullName: { $first: "$fullName" },
+          username: { $first: "$username" },
+          image: { $first: "$image" },
+          auctions: {
+            $push: {
+              auctionId: "$auctionId",
+              points: "$points",
+              rank: "$rank",
+            },
           },
         },
-        { $sort: { totalPoints: -1 } },
-        { $limit: limit }
-      ])
-      .toArray();
+      },
+      { $sort: { totalPoints: -1 } },
+      { $limit: limit },
+    ]);
 
     return NextResponse.json(leaderboardData);
   } catch (error) {
-    console.error('Error fetching leaderboard data:', error);
-    return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
+    console.error("Error fetching leaderboard data:", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
   }
 }
-
 
 export async function PUT(req: NextRequest) {
   try {
@@ -40,7 +49,7 @@ export async function PUT(req: NextRequest) {
     const db = client.db();
     const { userID, auctionID, points } = await req.json();
 
-    const updateResult = await db.collection('auction_points').updateOne(
+    const updateResult = await db.collection("auction_points").updateOne(
       {
         userID: new mongoose.Types.ObjectId(userID),
         auctionID: new mongoose.Types.ObjectId(auctionID),
@@ -51,11 +60,17 @@ export async function PUT(req: NextRequest) {
     );
 
     if (updateResult.modifiedCount === 0) {
-      return NextResponse.json({ message: 'No changes made or item not found' }, { status: 404 });
+      return NextResponse.json(
+        { message: "No changes made or item not found" },
+        { status: 404 }
+      );
     }
-    return NextResponse.json({ message: 'Points updated successfully' });
+    return NextResponse.json({ message: "Points updated successfully" });
   } catch (error) {
-    console.error('Error updating auction points:', error);
-    return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
+    console.error("Error updating auction points:", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
   }
 }
