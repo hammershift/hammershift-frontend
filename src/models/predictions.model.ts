c1105c7a9da11771a899c16fcefba5592e1d7eae
Ramon Arca <ramonteodoroarca@gmail.com>
2025-04-22 22:36:15 +0800

    Update predictions.model.ts

--- src/models/predictions.model.ts
+++ src/models/predictions.model.ts
@@ -14,12 +14,18 @@ export interface Prediction {
 const predictionsSchema = new Schema(
   {
     carId: { type: String, required: true },
+    carObjectId: { type: Types.ObjectId, required: true },
     predictedPrice: { type: Number, required: true },
     predictionType: { type: String, required: true },
+    wagerAmount: { type: Number, required: false, default: 0 },
     user: {
+      userId: { type: Types.ObjectId, required: true },
       fullName: { type: String, required: true },
       username: { type: String, required: true },
     },
+    refunded: { type: Boolean, required: false, default: false },
+    isActive: { type: Boolean, required: true, default: true },
+    prize: { type: Number, required: false, default: 0 },
   },
   {
     collection: "predictions",
