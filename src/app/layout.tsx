7b6d602ff4d0554aeb695a83bf4e4d88921db108
Seaver Choy <seaver@edamama.ph>
2025-05-03 14:09:59 +0800

    Fix build issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import Footer from "./components/footer";
 import Navbar from "./components/navbar";
-import { PredictionProvider } from "./context/predictionContext";
+import { PredictionProvider } from "./context/PredictionContext";
 import "./styles/app.css";
 import "./styles/globals.css";
 
