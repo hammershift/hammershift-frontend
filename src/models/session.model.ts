b4f3ae0c18b22eb6ec495a966eed35ec90eb61cf
Ramon Arca <ramonteodoroarca@gmail.com>
2025-05-21 00:07:06 +0800

    Add session model

---
+++ src/models/session.model.ts
@@ -0,0 +1,23 @@
+import mongoose, { Document, Schema, model, models, Types } from "mongoose";
+
+export interface Session extends Document {
+  _id: Types.ObjectId;
+  userId: Types.ObjectId;
+  expiresAt: Date;
+  ipAddress: string;
+  userAgent: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const sessionSchema = new Schema({
+  _id: { type: Schema.Types.ObjectId, required: true },
+  userId: { type: Schema.Types.ObjectId, required: true },
+  expiresAt: { type: Date, required: true },
+  ipAddress: { type: String, required: true },
+  userAgent: { type: String, required: true },
+});
+
+const Sessions = models.sessions || model("sessions", sessionSchema);
+
+export default Sessions;
