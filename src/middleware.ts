7c9fd24bee0b244b27d650f727293470d0fe70fa
Ramon Arca <ramonteodoroarca@gmail.com>
2025-05-22 10:57:20 +0800

    Create middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse, NextRequest } from "next/server";
+import { betterFetch } from "@better-fetch/fetch";
+import type { auth } from "@/lib/betterAuth";
+
+type Session = typeof auth.$Infer.Session;
+export async function middleware(req: NextRequest) {
+  const session = await betterFetch<Session>("/api/auth/get-session", {
+    baseURL: req.nextUrl.origin,
+    headers: {
+      cookie: req.headers.get("cookie") || "",
+    },
+  });
+
+  if (!session || !session.data) {
+    return NextResponse.redirect(new URL("/login_page", req.url));
+  }
+
+  return NextResponse.next({
+    headers: {
+      "x-user-id": session.data.user.id,
+      "x-user-username": session.data.user.username!,
+    },
+  });
+}
+
+export const config = {
+  matcher: ["/api/comments/:path", "/api/userInfo/:path"],
+};
