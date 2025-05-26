edb66caeeee4db805ae9c046383ffab123bd5798
Ramon Arca <ramonteodoroarca@gmail.com>
2025-05-26 12:48:37 +0800

    Email templates

--- src/app/components/email-template.tsx
+++ src/app/components/email-template.tsx
@@ -1,11 +1,18 @@
 interface EmailTemplateProps {
-  username: string;
-  url: string;
+  name?: string;
+  url?: string;
 }
 
-export const EmailTemplate = ({ username, url }: EmailTemplateProps) => (
+export const VerifyEmailTemplate = ({ name, url }: EmailTemplateProps) => (
   <div>
-    <h1>Welcome to Velocity Markets, {username}!</h1>
+    <h1>Welcome to Velocity Markets, {name}!</h1>
     <p>Click the link to verify your email: {url}</p>
+  </div>
+);
+
+export const ResetPasswordEmailTemplate = ({ url }: EmailTemplateProps) => (
+  <div>
+    <h1>Reset Password</h1>
+    <p>Click the link to reset your password: {url}</p>
   </div>
 );
